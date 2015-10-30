c6eff4674ead8d74993d9a8a0469bf4fab5d6332
Kapil Thangavelu <kapil.thangavelu@capitalone.com>
2015-10-30 09:55:51 -0400

    fixup identify output

--- janitor/commands.py
+++ janitor/commands.py
@@ -1,23 +1,17 @@
 import os
 import sys
 
-def _serialize(options, manager):
+
+def identify(options, policy_collection):
     if options.output_path == "-":
         fh = sys.stdout
     else: # dangling fh on close
         fh = open(os.path.expanduser(options.output_path), 'w')
 
-    if options.format == "json":
-        manager.format_json(manager.resources(), fh)
-    else:
-        manager.format_csv(manager.resources(), fh)
-    
-
-def identify(options, policy_collection):
     for policy in policy_collection:
         manager = policy.resource_manager
         resources = manager.resources()
-        print manager.format_json(resources)
+        print manager.format_json(resources, fh)
 
     
 def run(options, policy_collection):
