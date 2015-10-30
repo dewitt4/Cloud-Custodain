9f65306510e7d07f8be0635717dcd0ef513359c5
Kapil Thangavelu <kapil.thangavelu@capitalone.com>
2015-10-30 09:57:16 -0400

    fixup identify output

--- janitor/commands.py
+++ janitor/commands.py
@@ -3,10 +3,8 @@ import sys
 
 
 def identify(options, policy_collection):
-    if options.output_path == "-":
-        fh = sys.stdout
-    else: # dangling fh on close
-        fh = open(os.path.expanduser(options.output_path), 'w')
+
+    fh = sys.stdout
 
     for policy in policy_collection:
         manager = policy.resource_manager
