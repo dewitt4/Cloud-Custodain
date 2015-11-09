2d88f00d117f79075da9b65e0851e678e0afae08
Kapil Thangavelu <kapil.thangavelu@capitalone.com>
2015-11-09 06:41:18 -0500

    sqs concurrent.futures abstraction wip

---
+++ ftests/test_sqs.py
@@ -0,0 +1,39 @@
+
+import boto3
+import logging
+import os
+import random
+import string
+import unittest
+
+from janitor.sqsexec import SQSExecutor
+
+TEST_SQS_PREFIX = os.environ.get("TEST_SQS_PREFIX", "cloud-maid-ftest")
+
+
+def session_factory():
+    return boto3.Session()
+
+
+class SQSExecFunctional(unittest.TestCase):
+
+    def setUp(self):
+        logging.getLogger('botocore').setLevel(logging.WARNING)
+        self.session = session_factory()
+        self.client = self.session.client('sqs')
+        self.map_queue = self.client.create_queue(
+            QueueName = "%s-map-%s" % (
+                TEST_SQS_PREFIX, "".join(random.sample(string.letters, 3))))[
+                    'QueueUrl']
+        self.addCleanup(self.client.delete_queue, QueueUrl=self.map_queue)
+        self.reduce_queue = self.client.create_queue(
+            QueueName = "%s-map-%s" % (
+                TEST_SQS_PREFIX, "".join(random.sample(string.letters, 3))))[
+                    'QueueUrl']
+        self.addCleanup(self.client.delete_queue, QueueUrl=self.reduce_queue)        
+
+    def test_executor(self):
+        with SQSExecutor(session_factory, self.map_queue, self.reduce_queue) as w:
+            results = w.map(sum, [1,2,3,4])
+            print list(results)
+        
