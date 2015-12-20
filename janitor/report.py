06dd5b2507d19cc623a07e0a5d19cb6871a4188b
Kapil Thangavelu <kapil.thangavelu@capitalone.com>
2015-12-20 07:26:18 -0500

    flush out report command, yank blame command, cleanup docs a bit

--- janitor/report.py
+++ janitor/report.py
@@ -1,7 +1,25 @@
 """
-Reporting tools against cloud-maid's json output
-records
+Reporting tools against cloud-maid's json output records.
+
+For each policy execution maid stores structured output
+in json format of the records a policy has filtered to
+in an s3 bucket.
+
+These represent the records matching the policy filters
+that the policy will apply actions to.
+
+The reporting mechanism here simply fetches those records
+over a given time interval and constructs a resource type
+specific report on them.
+
+TODO
+
+The type specific formatting needs easy customization, 
+a config file for the report or maid, or named formats
+with format spec on the cli are all under consideration.
 """
+
+from concurrent.futures import as_completed
 
 from cStringIO import StringIO
 import csv
@@ -12,20 +30,14 @@ import logging
 
 from dateutil.parser import parse as date_parse
 
-from janitor.executor import ThreadPoolExecutor
+from janitor.executor import ThreadPoolExecutor, MainThreadExecutor
 from janitor.output import parse_s3, s3_join
 from janitor.utils import local_session
 
 log = logging.getLogger('maid.reports')
 
 
-def report(
-        session_factory,
-        policy,
-        start_date,
-        output_fh,
-        s3_record_path,
-        filters=None):
+def report(policy, start_date, output_fh, filters=None):
     """Format a policy's extant records into a report."""
     formatter = RECORD_TYPE_FORMATTERS.get(policy.resource_type)
 
@@ -34,8 +46,12 @@ def report(
             "No formatter for resource type %s, valid: %s" % (
                 policy.resource_type, ", ".join(RECORD_TYPE_FORMATTERS)))
 
-    records = record_set(session_factory, s3_record_path, start_date)
-
+    records = record_set(
+        policy.session_factory,
+        policy.ctx.output.bucket,
+        policy.ctx.output.key_prefix,
+        start_date)
+    
     original_record_count = len(records)    
     if filters:
         for f in filters:
@@ -49,7 +65,7 @@ def report(
 
 def ec2_csv(output_fh, records):
     headers = ['instance-id', 'name',
-               'instance-type', 'launch', 'vpc-id',
+               'instance-type', 'launch', 'vpc-id', 'ip-addr',
                'asv', 'env', 'owner']
     writer = csv.writer(output_fh, headers)
     writer.writerow(headers)
@@ -62,6 +78,7 @@ def ec2_csv(output_fh, records):
             r['InstanceType'],
             r['LaunchTime'],
             r['VpcId'],
+            r['PrivateIpAddress'],
             tag_map.get("ASV", ""),
             tag_map.get("CMDBEnvironment", ""),
             tag_map.get("OwnerContact", "")])
@@ -89,14 +106,13 @@ RECORD_TYPE_FORMATTERS = {
     }
 
 
-def record_set(session_factory, s3_url, start_date):
+def record_set(session_factory, bucket, key_prefix, start_date):
     """Retrieve all s3 records for the given policy output url
 
     From the given start date.
     """
 
     s3 = local_session(session_factory).client('s3')
-    _, bucket, key_prefix = parse_s3(s3_url)
     marker = key_prefix.strip("/") + "/" + start_date.strftime(
         '%Y-%m-%d-00') + "/resources.json.gz"
 
@@ -116,23 +132,31 @@ def record_set(session_factory, s3_url, start_date):
             keys = [k for k in key_set['Contents']
                     if k['Key'].endswith('resources.json.gz')]
             key_count += len(keys)
-            map(records.extend,
-                w.map(get_records,
-                      zip(keys, itertools.repeat(session_factory))))
+            futures = []
+            for k in keys:
+                futures.append(
+                    w.submit(get_records, bucket, k, session_factory))
+
+            for f in as_completed(futures):
+                records.extend(f.result())
+
     log.info("Fetched %d records across %d files" % (
         len(records), key_count))
     return records
 
 
-def get_records(ctx):
-    session_factory, bucket, key = ctx
+def get_records(bucket, key, session_factory):
     # we're doing a lot of this in memory, worst case
     # though we're talking about a 10k objects, else
     # we should spool to temp files
+    _, date_str, _ = key['Key'].rsplit("/", 2)
+    maid_date = date_parse(date_str)
     s3 = local_session(session_factory).client('s3')
-    result = s3.get_object(Bucket=bucket, Key=key)
+    result = s3.get_object(Bucket=bucket, Key=key['Key'])
     blob = StringIO(result['Body'].read())
-    records = json.loads(gzip.GzipFile(fileobj=blob))
+    records = json.load(gzip.GzipFile(fileobj=blob))
+    for r in records:
+        r['MaidDate'] = maid_date
     return records
 
         
