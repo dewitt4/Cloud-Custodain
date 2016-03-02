e42028c4479f2dcedfa4d286b8f77584a40b7444
Kapil Thangavelu <kapil.thangavelu@capitalone.com>
2016-03-02 07:41:36 -0500

    checkpoint wip on elb filters

--- janitor/resources/elb.py
+++ janitor/resources/elb.py
@@ -3,12 +3,11 @@ Elastic Load Balancers
 ----------------------
 
 
-Common Policies
-###############
+TODO
+####
 
-SSL Policy enforcement
-
-Empty instance waste collection
+- SSL Policy enforcement
+- Empty instance waste collection
 
 Actions
 #######
@@ -35,24 +34,19 @@ In addition to value filters
 .. code-block:: yaml
 
   filters:
-    - type: ssl-policy:
-      name: []
-      whitelist: []
-      blacklist:
-      - "Protocol-SSLv2"
-
-
-  filters:
-    - type: healthcheck-protocol-mismatch:
+    # Matches when the backend listener and health check are
+    # not on the same protocol
+    - healthcheck-protocol-mismatch
 
 """
+from concurrent.futures import as_completed
 import logging
 import itertools
 
 from janitor.actions import ActionRegistry, BaseAction
 from janitor.filters import Filter, FilterRegistry
 from janitor.manager import ResourceManager, resources
-from janitor.utils import local_session
+from janitor.utils import local_session, chunks
 
 log = logging.getLogger('maid.elb')
 
@@ -73,10 +67,6 @@ class ELB(ResourceManager):
 
     def resources(self):
         c = self.session_factory().client('elb')
-        query = self.resource_query()  # FIXME: This is not used.
-        # paginator for ELB only takes load balancer names and a few other params,
-        # not a generic query
-        self.log.info("Querying elb instances")
         p = c.get_paginator('describe_load_balancers')
         results = p.paginate()
         elbs = list(itertools.chain(
@@ -92,22 +82,99 @@ class Delete(BaseAction):
             list(w.map(self.process_elb, load_balancers))
 
     def process_elb(self, elb):
-        client = local_session(
-            self.manager.session_factory).client('elb')
-        client.delete_load_balancer(
-            LoadBalancerName=elb['LoadBalancerName'])
+        client = local_session(self.manager.session_factory).client('elb')
+        client.delete_load_balancer(LoadBalancerName=elb['LoadBalancerName'])
+
+
+@filters.register('ssl-policy')
+class SSLPolicyFilter(Filter):
+    """Filter ELBs on the properties of SSLNegotation policies.
+    TODO: Only works on custom policies at the moment.
+    filters:
+      - type: ssl-policy
+        blacklist:
+        - "Protocol-SSLv2"
+        - "Protocol-SSLv3"
+    """
+    
+    def process(self, balancers):
+        balancers = [b for b in balancers if self.is_ssl(b)]
+        balancer_policy_map = {}
+        policy_attr_map = {}
+        
+        for b in balancers:
+            for ld in b['ListenerDescriptions']:
+                if ld['Listener']['Protocol'] != 'HTTPS':
+                    continue
+                for p in ld['PolicyNames']:
+                    # Skip precanned
+                    if p.startswith('ELBSecurityPolicy'):
+                        continue
+                    elif p.startswith('ELBSample'):
+                        continue
+                    balancer_policy_map.setdefault(
+                        b['LoadBalancerName'], []).apppend(p)
+
+        with self.executor_factory(max_workers=3) as w:
+            futures = []
+            
+            for elb_policy_set in chunks(balancer_policy_map.items(), 50):
+                futures.append(
+                    w.submit(self.process_elb_policy_set, elb_policy_set))
+
+                for f in as_completed(futures):
+                    if f.exception():
+                        self.log.error(
+                            "Exception processing elb policies \n %s" % (
+                                f.exception()))
+                        continue
+                for elb_policies in f.result():
+                    pass
+        
+    def process_elb_policy_set(self, elb_policy_set):
+        results = []        
+        client = local_session(self.manager.session_factory).client('elb')
+        
+        for (elb_name, policy_names) in elb_policy_set:
+            policies = client.describe_load_balancer_policies(
+                LoadBalancerName=elb_name,
+                PolicyNames=policy_names)['PolicyDescriptions']
+            lb_policies = []
+            for p in policies:
+                if p['PolicyTypeName'] != 'SSLNegotiationPolicyType':
+                    lb_policies.append(
+                        {'Name': p['PolicyName'],
+                         'Type': p['PolicyTypeName'],
+                         'Attrs': {a['AttributeName']: a['AttributeValue']
+                                   for a in p['PolicyAttributeDescriptions']}})
+            results.append((elb_name, lb_policies))
+        return results
+                
+            
+    @staticmethod
+    def is_ssl(b):
+        for ld in b['ListenerDescriptions']:
+            if ld['Listener']['Protocol'] != 'HTTPS':
+                continue
+            return True
+        return False        
 
 
 @filters.register('healthcheck-protocol-mismatch')
 class HealthCheckProtocolMismatch(Filter):
-
+    """
+    """
     def __call__(self, load_balancer):
         health_check_protocol = load_balancer['HealthCheck']['Target'].split(':')[0]
         listener_descriptions = load_balancer['ListenerDescriptions']
+        
         if len(listener_descriptions) == 0:
             return True
 
-        # check if any of the protocols in the ELB match the health check. There is only 1 health
-        # check, so if there are multiple listeners, we only check if at least one of them matches
-        protocols = [ listener['Listener']['InstanceProtocol'] for listener in listener_descriptions ]
+        # check if any of the protocols in the ELB match the health
+        # check. There is only 1 health check, so if there are
+        # multiple listeners, we only check if at least one of them
+        # matches
+        protocols = [listener['Listener']['InstanceProtocol']
+                     for listener in listener_descriptions]
         return health_check_protocol in protocols
--- tests/test_elb.py
+++ tests/test_elb.py
@@ -1,25 +1,26 @@
-
-import mock
-
-from janitor.resources.elb import ELB
-
 from .common import BaseTest
 
 
-class ResourceManagerTest(BaseTest):
-
-    def setUp(self):
-        self.client = mock.Mock()
-        self.session_factory = mock.Mock()
-        self.session_factory.client.return_value = self.client
-        self.session_factory.start()
-
-    def tearDown(self):
-        self.session_factory.stop()
-        
-    def xtest_resources(self):
-        mgr = ELB(self.get_context(session_factory=self.session_factory), {})
-        self.assertEqual(mgr.resources(), [])
-
-
+class HealthCheckProtocolMismatchTest(BaseTest):
     
+    def test_healthcheck_protocol_mismatch(self):
+        session_factory = self.replay_flight_data(
+            'test_healthcheck_protocol_mismatch')
+        policy = self.load_policy({
+            'name': 'healthcheck-protocol-mismatch',
+            'resource': 'elb',
+            'filters': [
+                {'type': 'healthcheck-protocol-mismatch'}
+            ]},
+            session_factory=session_factory)
+        
+        resources = policy.run()
+        self.assertEqual(len(resources), 3)
+
+        # make sure we matched the right load balcners
+        elb_names = set([elb['LoadBalancerName'] for elb in resources])
+        self.assertEqual(
+            elb_names, set(
+                ['test-elb-no-listeners',
+                 'test-elb-protocol-matches',
+                 'test-elb-multiple-listeners']))
