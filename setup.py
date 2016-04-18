c9ec44c531a9c896bdcb7b3fc5211c37c7787025
Kapil Thangavelu <kapilt@gmail.com>
2016-04-18 06:17:54 -0400

    trove classifiers, markdown support for pypi (#1)

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
 from setuptools import setup, find_packages
 
+
 setup(
     name="c7n",
-    description="Cloud Custodian - Policy Rules Engine",
     version='0.8.2',
+    description="Cloud Custodian - Policy Rules Engine",
+    long_description_markdown_filename='README.md',
+    classifiers=[
+      "Topic :: System :: Systems Administration",
+      "Topic :: System :: Distributed Computing"
+    ],
+    url="https://github.com/capitalone/cloud-custodian",
+    license="Apache-2.0",
     packages=find_packages(),
     entry_points={
         'console_scripts': [
