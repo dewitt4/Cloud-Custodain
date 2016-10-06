257e1a782b2014938fc212b060da8647784c6e32
Joshua Root <joshua.root@gmail.com>
2016-10-06 19:56:08 -0400

    Custodian ec2recovery (#480)
    * EC2 Recovery Update
    
    * Corrected shutdown ec2 before dismount on restoring volumes
    
    * tag copy update
    
    * Added check to copy tags from snapshot if no instance is present

--- tools/ec2recovery.py
+++ tools/ec2recovery.py
@@ -1,6 +1,6 @@
 from datetime import datetime
-import boto3
-import botocore.exceptions as bex
+from boto3.session import Session
+from botocore.exceptions import ClientError
 import argparse
 import sys
 import os
@@ -21,37 +21,44 @@ def generate_argparser():
     parser = argparse.ArgumentParser()
     subs = parser.add_subparsers()
     lists = subs.add_parser('list',
-                            description='List snapshot for a given ec2 instance')
+                            description='List snapshot for a '
+                                        'given ec2 instance')
+    lists.set_defaults(which='list')
     configure_subparser(lists)
 
     restore = subs.add_parser('restore',
-                              description='Restore EBS volume(s) from snapshot(s)')
+                              description='Restore EBS volume(s) from '
+                                          'snapshot(s)')
+    restore.set_defaults(which='restore')
     configure_subparser(restore)
     restore.add_argument('--snapshots',
                          help='Comma-delimited string of snapshot '
                               'Ids to restore volumes from')
 
     rebuild = subs.add_parser('rebuild',
-                              description='Rebuild an EC2 instance from snapshot(s)')
+                              description='Rebuild an EC2 '
+                                          'instance from snapshot(s)')
+    rebuild.set_defaults(which='rebuild')
     configure_subparser(rebuild)
-    rebuild.add_argument('--ami', help='AMI ID to restore from (required'
-                                       'when performing full restore')
-    rebuild.add_argument('--snapshots',
+    rebuild.add_argument('--ami', required=True,
+                         help='AMI ID to restore from '
+                              '(required when performing full restore')
+    rebuild.add_argument('--snapshot', action='append', required=True,
                          help='Comma-delimited string of'
                               ' snapshot IDs to restore')
-    rebuild.add_argument('--securitygroups',
+    rebuild.add_argument('--sg', action='append', required=True,
                          help='Comma-delimited string of security'
                               ' groups to which the new instance will'
                               ' be associated with')
-    rebuild.add_argument('--subnet', help='Subnet ID to associated'
-                                          ' to instance')
-    rebuild.add_argument('--rolename', help='IAM Role name to '
-                                            'associate to instance')
-    rebuild.add_argument('--keypair',
+    rebuild.add_argument('--subnet', required=True,
+                         help='Subnet ID to associated to instance')
+    rebuild.add_argument('--role', default='',
+                         help='IAM Role name to associate to instance')
+    rebuild.add_argument('--keypair', default='',
                          help='Name of SSH KeyPair to associate to instance')
     rebuild.add_argument('--type', help='EC2 instance type (size)')
-    restore.add_argument('--userdata', help='Path to userdata script '
-                                            'to run on new instance')
+    rebuild.add_argument('--userdata', default='',
+                         help='Path to userdata script to run on new instance')
     return parser
 
 
@@ -78,73 +85,34 @@ def validate_snapshots(dryrun, client, snapshots):
                     }
                     results.append(volume)
         return results
-    except bex.ClientError as e:
+    except ClientError as e:
         print "[Error]: %s" % e.response['Error']['Message']
         sys.exit(-1)
 
 
-def list_snapshots(dryrun, client, instance):
-    snapshots = client.describe_snapshots(
-        DryRun=dryrun,
+def list_snapshots(session, instance):
+    snapshots = session.client('ec2').describe_snapshots(
         Filters=[{
             'Name': 'description',
-            'Values': ["Automated,Backup,%s,*" %
-                       instance]}])['Snapshots']
+            'Values': ["Automated,Backup,%s,*" % (
+                instance)]}])['Snapshots']
     snapshot_map = {}
     for snapshot in snapshots:
         start_date = snapshot['StartTime'].date()
         snapshot_id = snapshot['SnapshotId']
         volume_id = snapshot['VolumeId']
-        description = snapshot['Description']
         if not start_date in snapshot_map:
             snapshot_map[start_date] = []
         for tag in snapshot['Tags']:
             if 'DeviceName' in tag['Key']:
                 snapshot_map[start_date].append([
                     volume_id, tag['Value'],
-                    snapshot_id, description])
+                    snapshot_id])
     for snapshot in snapshot_map:
-        print "\nSnapshot Date: %s" % snapshot
+        print "\n\tSnapshot Date: %s" % snapshot
         for value in snapshot_map[snapshot]:
-            print "\t VolumeId: %s (%s), SnapshotId: %s, Description: %s" % (
-                value[0], value[1],
-                value[2], value[3])
-
-
-def create_tags(tags):
-    result = []
-    if tags:
-        for i in range(len(tags.split(','))):
-            pairs = tags[i].split(':')
-            key = pairs[0]
-            value = pairs[1]
-            result.append({
-                'Key': key,
-                'Value': value})
-    result.append({
-        'Key': 'RestoreDate',
-        'Value': datetime.now().strftime("%Y-%m-%d %H:%M:%S")})
-    return result
-
-
-def clone_tags(dryrun, ec2, old_id, new_id):
-    tags = []
-    tags.append({
-        'Key': 'RestoreDate',
-        'Value': datetime.now().strftime("%Y-%m-%d %H:%M:%S")})
-    old_ec2 = ec2.Instance(old_id)
-    for tag in old_ec2.tags:
-        key = tag['Key']
-        val = tag['Value']
-        if 'aws:' in key:
-            continue
-        if key == 'Name':
-            val += "-Restored"
-        tags.append({'Key' : key, 'Value': val})
-    ec2.Instance(new_id).create_tags(
-        DryRun=dryrun,
-        Tags=tags
-    )
+            print "\t  - VolumeId: %s (%s), SnapshotId: %s" % (
+                value[0], value[1], value[2])
 
 
 def read_user_date(userdata):
@@ -155,167 +123,152 @@ def read_user_date(userdata):
         return input.readlines()
 
 
-def restore(dryrun, session, instanceid, snapshots):
-    res = session.resource('ec2')
-    instance = res.Instance(instanceid)
-    if instance.state['Name'] == 'running':
-        print "Stopping instance..."
-        instance.stop()
-        waiter = session.client('ec2').get_waiter('instance_stopped')
-        waiter.wait(
-            DryRun=dryrun,
-            InstanceIds=[instanceid]
-        )
-    print "Creating & mounting new volumes from snapshot set..."
-    for snapshot in snapshots:
-        device_name = snapshot['DeviceName']
-        snapshot_id = snapshot['Ebs']['SnapshotId']
+def excessive_volume_waiter(r, volumeid, status):
+    """
+        Using this method to wait for a volume to detatch as the existing
+        waiters are continuously timing out
+    """
+    import time
+    while r.Volume(volumeid).state != status:
+        time.sleep(15)
 
-        volume_id = res.Snapshot(snapshot_id).volume_id
-        volumes = instance.block_device_mappings
-        for volume in volumes:
-            if volume['DeviceName'] == device_name:
-                volume_id = volume['Ebs']['VolumeId']
-        volume_az = res.Volume(volume_id).availability_zone
-        tags = res.Volume(volume_id).tags
+
+def validate_snapshot(snapshot):
+    results = {}
+    for tag in snapshot.tags:
+        if tag['Key'] == 'DeviceName':
+            results['DeviceName'] = tag['Value']
+        if tag['Key'] == 'Name' or tag['Key'] == 'VolumeId':
+            results['VolumeId'] = tag['Value']
+        if tag['Key'] == 'AvailabilityZone':
+            results['AvailabilityZone'] = tag['Value']
+    return results
+
+
+def restore_volume(dryrun, session, instanceid, snapshots):
+    r = session.resource('ec2')
+    instance = r.Instance(instanceid)
+    if instance.state['Name'] == 'running':
+        instance.stop()
+        session.client('ec2').get_waiter('instance_stopped').wait(
+            DryRun=dryrun, InstanceIds=[instanceid])
+    for snapshot in snapshots:
+        snap = r.Snapshot(snapshot)
+        tags = [t for t in snap.tags if 'aws:' not in t['Key']]
+        values = validate_snapshot(snap)
+        if not values:
+            continue
+        devname = values['DeviceName']
+        volid = values['VolumeId']
+        vol = r.Volume(volid)
+        if not 'AvailabilityZone' in values:
+            az = vol.availability_zone
+            tags.append({'Key': 'AvailabilityZone', 'Value': az})
+        else:
+            az = values['AvailabilityZone']
 
         waiter = session.client('ec2').get_waiter('volume_available')
-        new_volume = res.create_volume(
-            DryRun=dryrun,
-            SnapshotId=snapshot_id,
-            AvailabilityZone=volume_az
-        )
-        waiter.wait(
-            DryRun=dryrun,
-            VolumeIds=[new_volume.volume_id]
-        )
+        for d in instance.block_device_mappings:
+            if d['DeviceName'] != devname:
+                continue
+            instance.detach_volume(
+                DryRun=dryrun, VolumeId=d['Ebs']['VolumeId'], Force=True)
+            excessive_volume_waiter(r, d['Ebs']['VolumeId'], 'available')
 
-        instance.detach_volume(
-            DryRun=dryrun,
-            VolumeId=volume_id,
-            Force=True
-        )
-        waiter.wait(
-            DryRun=dryrun,
-            VolumeIds=[volume_id]
-        )
+        newvol = r.create_volume(
+            DryRun=dryrun, SnapshotId=snapshot, AvailabilityZone=az)
+        waiter.wait(DryRun=dryrun, VolumeIds=[newvol.volume_id])
 
-        print "Created volume %s (%s) from snapshot %s" % (
-            new_volume.volume_id,
-            device_name,
-            snapshot_id)
         waiter = session.client('ec2').get_waiter('volume_in_use')
-        res.Volume(new_volume.volume_id).attach_to_instance(
-            DryRun=dryrun,
-            InstanceId=instanceid,
-            Device=device_name
-        )
-        waiter.wait(
-            DryRun=dryrun,
-            VolumeIds=[new_volume.volume_id]
-        )
+        r.Volume(newvol.volume_id).attach_to_instance(
+            DryRun=dryrun, InstanceId=instanceid, Device=devname)
+        waiter.wait(DryRun=dryrun, VolumeIds=[newvol.volume_id])
+
         if tags:
-            res.Volume(new_volume.volume_id).create_tags(
-                DryRun=dryrun,
-                Tags=tags
-            )
-        destroy_volume(dryrun, session, volume_id)
-    print "Starting instance..."
+            r.Volume(newvol.volume_id).create_tags(
+                DryRun=dryrun,Tags=tags)
     instance.start()
     waiter = session.client('ec2').get_waiter('instance_running')
-    waiter.wait(
-        DryRun=dryrun,
-        InstanceIds=[instanceid]
-    )
+    waiter.wait(DryRun=dryrun, InstanceIds=[instanceid])
 
 
-def destroy_volume(dryrun, session, volume_id):
-    session.resource(
-        'ec2').Volume(
-        volume_id).delete(
-        DryRun=dryrun)
-
-
-def rebuild(session, dryrun, ami, instanceid,
-            keyname, securitygroups, type,
-            subnet, snapshots, iamrolename,
-            userdata):
+def copy_tags(session, oldid, newid):
     try:
-        ec2 = session.resource('ec2')
-        client = session.client('ec2')
-        instances = ec2.create_instances(
+        c = session.client('ec2')
+        tags = c.describe_tags(
+            Filters=[{
+                'Name': 'resource-id',
+                'Values': [oldid]}])['Tags']
+        tags = [t for t in oval.tags if 'aws:' not in t['Key']]
+        tags.append({
+            'Key': 'RecoveryDate',
+            'Value': datetime.now().strftime('%m/%d/%Y')})
+        c.create_tags(
+            Resources=[newid],
+            Tags=tags)
+    except:
+        return False
+
+
+def rebuild_instance(dryrun, session, ami, instanceid, keypair, sgs, type,
+            subnet, snapshots, role, userdata):
+
+    try:
+        c = session.client('ec2')
+        instance = c.run_instances(
             DryRun=dryrun,
             ImageId=ami,
             MinCount=1,
             MaxCount=1,
-            KeyName=keyname,
-            SecurityGroupIds=securitygroups.split(','),
+            KeyName=keypair,
+            SecurityGroupIds=sgs,
             InstanceType=type,
             SubnetId=subnet,
-            BlockDeviceMappings=snapshots,
-            IamInstanceProfile={'Name': iamrolename},
-            UserData=userdata)
-        instance_id = instances[0].instance_id
-        print "\n\tRestoring instance from snapshot..."
+            IamInstanceProfile={'Name': role},
+            UserData=userdata)['Instances'][0]
 
-        waiter = client.get_waiter('instance_running')
-        waiter.wait(
-            DryRun=dryrun,
-            InstanceIds=[instance_id]
-        )
-        clone_tags(dryrun, ec2, instanceid, instance_id)
-        print "New instance '%s' created." % instance_id
-    except bex.ClientError as ce:
-        print ce.response['Error']['Message']
+        if instanceid:
+            copy_tags(session, instanceid, instance['InstanceId'])
+        else:
+            copy_tags(session, snapshots[0], instance['InstanceId'])
+
+        c.get_waiter('instance_running').wait(
+            DryRun=dryrun, InstanceIds=[instance['InstanceId']])
+
+        c.stop_instances(
+            DryRun=dryrun, InstanceIds=[instance['InstanceId']])
+        c.get_waiter('instance_stopped').wait(
+            DryRun=dryrun, InstanceIds=[instance['InstanceId']])
+
+        print "New instance created. Instance Id: %s (%s)" % (
+            instance['InstanceId'], instance['PrivateIpAddress'])
+        print "Restoring volumes..."
+        restore_volume(dryrun, session, instance['InstanceId'], snapshots)
+    except ClientError as e:
+        print e.response['Error']['Message']
 
 
 def main():
     parser = generate_argparser()
     args = parser.parse_args()
-    session = boto3.Session(
-        aws_access_key_id=args.accesskey,
-        aws_secret_access_key=args.secretkey,
-        profile_name=args.profile,
-        region_name=args.region)
-    client = session.client('ec2')
+    if args.profile:
+        session = Session(profile_name=args.profile, region_name=args.region)
+    elif args.accesskey and args.secretkey:
+        session = Session(
+            aws_access_key_id=args.accesskey,
+            aws_secret_access_key=args.secretkey,
+            region_name=args.region)
+    else:
+        return
 
-    # List all snapshots associated
-    # associated to the instance
-    # volumes
-    if sys.argv[1] == 'list':
-        if not args.instanceid:
-            print "An instance ID is required to list snapshots";
-            sys.exit(-1)
-        list_snapshots(
-            args.dryrun,
-            client,
-            args.instanceid)
-        sys.exit(0)
-
-    snapshots = validate_snapshots(
-        args.dryrun,
-        client,
-        args.snapshots)
-
-    if sys.argv[1] == 'restore':
-        restore(args.dryrun, session,
-                args.instanceid, snapshots)
-
-    if sys.argv[1] == 'rebuild':
-        try:
-            userdata = read_user_date(args.userdata)
-        except:
-            userdata = ""
-
-        if userdata:
-            print "Use of the 'userdata' feature does not guarantee " \
-                  "that the restored instance will run the userdata. " \
-                  "This feature is experimental."
-
-        rebuild(session, args.dryrun, args.ami, args.instanceid,
-                args.keypair, args.securitygroups,
-                args.type, args.subnet,
-                snapshots, args.rolename, userdata)
+    if args.which == 'list':
+        list_snapshots(session, args.instanceid)
+    elif args.which == 'restore':
+        restore_volume(args.dryrun, session, args.instanceid, args.snapshots)
+    elif args.which == 'rebuild':
+        rebuild_instance(args.dryrun, session, args.ami, args.instanceid,
+                         args.keypair, args.sg, args.type, args.subnet,
+                         args.snapshot, args.role, args.userdata)
 
 
 if __name__ == '__main__':
