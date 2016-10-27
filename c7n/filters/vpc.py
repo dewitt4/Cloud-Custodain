6050e58f135868a29069c4aac1705c7d4bb24eb3
Rob McBroom <rob@sixfeetup.com>
2016-10-27 13:01:28 -0400

    don't set default_vpc to an empty string
    the method is checking for None
    
    fixes #552

--- c7n/filters/vpc.py
+++ c7n/filters/vpc.py
@@ -52,8 +52,6 @@ class DefaultVpcBase(Filter):
             vpcs = [v['VpcId'] for v
                     in client.describe_vpcs(VpcIds=[vpc_id])['Vpcs']
                     if v['IsDefault']]
-            if not vpcs:
-                self.default_vpc = ""
-            else:
+            if vpcs:
                 self.default_vpc = vpcs.pop()
         return vpc_id == self.default_vpc and True or False
