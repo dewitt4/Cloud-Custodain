0daca5bb20db694f54ef5515681c4d5c6fecedde
Kapil Thangavelu <kapilt@gmail.com>
2016-11-04 12:12:30 -0400

    cloud watch log parser 2 sentry (#577)

---
+++ tools/c7n_sentry/README.md
@@ -0,0 +1,10 @@
+
+# Custodian Sentry Integration
+
+Generic Cloud Watch Logs scanner/subscription that
+searches for tracebacks, extracts frames, and posts
+to sentry.
+
+Useful for any python code logging to cloud watch logs
+including lambdas.
+
---
+++ tools/c7n_sentry/setup.py
@@ -0,0 +1,33 @@
+# Copyright 2016 Capital One Services, LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from setuptools import setup
+
+setup(
+    name="c7n_sentry",
+    version='0.1',
+    description="Cloud Custodian - Salactus S3",
+    classifiers=[
+      "Topic :: System :: Systems Administration",
+      "Topic :: System :: Distributed Computing"
+    ],
+    url="https://github.com/capitalone/cloud-custodian",
+    license="Apache-2.0",
+    py_modules=['c7nsentry'],
+    entry_points={
+        'console_scripts': [
+            'c7n-sentry = c7n_sentry.c7nsentry:main']},
+    install_requires=["c7n"],
+)
+
