57cb326f69724b2db275060c5686a957701e727c
sivarohithm <sivarohithm@gmail.com>
2016-12-16 18:19:39 -0500

    added rds instance csv field record creation time (#726)

--- c7n/reports/csvout.py
+++ c7n/reports/csvout.py
@@ -281,6 +281,7 @@ class RDSFormatter(Formatter):
         return [
             record['DBInstanceIdentifier'],
             record.get('DBName', ''),
+            record['InstanceCreateTime'],
             record['StorageEncrypted'],
             record['PubliclyAccessible'],
             tag_map.get("ASV", ""),
