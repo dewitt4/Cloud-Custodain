576a77ed78dcd574603768dfc76798f37c51910a
Kapil Thangavelu <kapilt@gmail.com>
2017-04-25 07:50:14 -0400

    account trail action (#1098) from #881

--- c7n/resources/account.py
+++ c7n/resources/account.py
@@ -14,6 +14,8 @@
 """AWS Account as a custodian resource.
 """
 
+import json
+from botocore.exceptions import ClientError
 from datetime import datetime, timedelta
 
 from dateutil.parser import parse as parse_date
@@ -22,6 +24,7 @@ from dateutil.tz import tzutc
 from botocore.exceptions import ClientError
 
 from c7n.actions import ActionRegistry
+from c7n.actions import BaseAction
 from c7n.filters import Filter, FilterRegistry, ValueFilter
 from c7n.manager import ResourceManager, resources
 from c7n.utils import local_session, type_schema
@@ -434,3 +437,145 @@ class ServiceLimit(Filter):
             resources[0]['c7n:ServiceLimitsExceeded'] = exceeded
             return resources
         return []
+
+
+def cloudtrail_policy(original, bucket_name, account_id):
+    '''add CloudTrail permissions to an S3 policy, preserving existing'''
+    ct_actions = [
+        {
+            'Action': 's3:GetBucketAcl',
+            'Effect': 'Allow',
+            'Principal': {'Service': 'cloudtrail.amazonaws.com'},
+            'Resource': 'arn:aws:s3:::' + bucket_name,
+            'Sid': 'AWSCloudTrailAclCheck20150319',
+        },
+        {
+            'Action': 's3:PutObject',
+            'Condition': {
+                'StringEquals':
+                {'s3:x-amz-acl': 'bucket-owner-full-control'},
+            },
+            'Effect': 'Allow',
+            'Principal': {'Service': 'cloudtrail.amazonaws.com'},
+            'Resource': 'arn:aws:s3:::%s/AWSLogs/%s/*' % (
+                bucket_name, account_id
+            ),
+            'Sid': 'AWSCloudTrailWrite20150319',
+        },
+    ]
+    # parse original policy
+    if original is None:
+        policy = {
+            'Statement': [],
+            'Version': '2012-10-17',
+        }
+    else:
+        policy = json.loads(original['Policy'])
+    original_actions = [a.get('Action') for a in policy['Statement']]
+    for cta in ct_actions:
+        if cta['Action'] not in original_actions:
+            policy['Statement'].append(cta)
+    return json.dumps(policy)
+
+
+@actions.register('enable-cloudtrail')
+class EnableTrail(BaseAction):
+    """Enables logging on the trail(s) named in the policy
+
+    :Example:
+
+    .. code-block: yaml
+
+        policies:
+          - name: trail-test
+            description: Ensure CloudTrail logging is enabled
+            resource: account
+            actions:
+              - type: enable-cloudtrail
+                trail: mytrail
+                bucket: trails
+    """
+
+    permissions = (
+        'cloudtrail:CreateTrail',
+        'cloudtrail:DescribeTrails',
+        'cloudtrail:GetTrailStatus',
+        'cloudtrail:StartLogging',
+        'cloudtrail:UpdateTrail',
+        's3:CreateBucket',
+        's3:GetBucketPolicy',
+        's3:PutBucketPolicy',
+    )
+    schema = type_schema(
+        'enable-cloudtrail',
+        **{
+            'trail': {'type': 'string'},
+            'bucket': {'type': 'string'},
+            'multi-region': {'type': 'boolean'},
+            'global-events': {'type': 'boolean'},
+            'notify': {'type': 'string'},
+            'file-digest': {'type': 'boolean'},
+            'kms': {'type': 'boolean'},
+            'kms-key': {'type': 'string'},
+            'required': ('bucket',),
+        }
+    )
+
+    def process(self, accounts):
+        """Create or enable CloudTrail"""
+        session = local_session(self.manager.session_factory)
+        client = session.client('cloudtrail')
+        bucket_name = self.data['bucket']
+        trail_name = self.data.get('trail', 'default-trail')
+        multi_region = self.data.get('multi-region', True)
+        global_events = self.data.get('global-events', True)
+        notify = self.data.get('notify', '')
+        file_digest = self.data.get('file-digest', False)
+        kms = self.data.get('kms', False)
+        kms_key = self.data.get('kms-key', '')
+
+        s3client = session.client('s3')
+        s3client.create_bucket(Bucket=bucket_name)
+        try:
+            current_policy = s3client.get_bucket_policy(Bucket=bucket_name)
+        except ClientError:
+            current_policy = None
+
+        policy_json = cloudtrail_policy(
+            current_policy, bucket_name, self.manager.config.account_id)
+
+        s3client.put_bucket_policy(Bucket=bucket_name, Policy=policy_json)
+        trails = client.describe_trails().get('trailList', ())
+        if trail_name not in [t.get('Name') for t in trails]:
+            new_trail = client.create_trail(
+                Name=trail_name,
+                S3BucketName=bucket_name,
+            )
+            if new_trail:
+                trails.append(new_trail)
+                # the loop below will configure the new trail
+        for trail in trails:
+            if trail.get('Name') != trail_name:
+                continue
+            # enable
+            arn = trail['TrailARN']
+            status = client.get_trail_status(Name=arn)
+            if not status['IsLogging']:
+                client.start_logging(Name=arn)
+            # apply configuration changes (if any)
+            update_args = {}
+            if multi_region != trail.get('IsMultiRegionTrail'):
+                update_args['IsMultiRegionTrail'] = multi_region
+            if global_events != trail.get('IncludeGlobalServiceEvents'):
+                update_args['IncludeGlobalServiceEvents'] = global_events
+            if notify != trail.get('SNSTopicArn'):
+                update_args['SnsTopicName'] = notify
+            if file_digest != trail.get('LogFileValidationEnabled'):
+                update_args['EnableLogFileValidation'] = file_digest
+            if kms_key != trail.get('KmsKeyId'):
+                if not kms and 'KmsKeyId' in trail:
+                    kms_key = ''
+                update_args['KmsKeyId'] = kms_key
+            if update_args:
+                update_args['Name'] = trail_name
+                client.update_trail(**update_args)
--- tests/test_account.py
+++ tests/test_account.py
@@ -12,6 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from common import BaseTest
+from c7n.utils import local_session
+
+
+TRAIL = 'nosetest'
 
 import datetime
 from dateutil import parser
@@ -225,5 +229,62 @@ class AccountTests(BaseTest):
             session_factory=session_factory)
         resources = p.run()
         self.assertEqual(len(resources), 1)
+        
+    def test_create_trail(self):
+        factory = self.replay_flight_data('test_cloudtrail_create')
+        p = self.load_policy(
+            {
+                'name': 'trail-test',
+                'resource': 'account',
+                'actions': [
+                    {
+                        'type': 'enable-cloudtrail',
+                        'trail': TRAIL,
+                        'bucket': '%s-bucket' % TRAIL,
+                    },
+                ],
+            },
+            session_factory=factory,
+        )
+        p.run()
+        client = local_session(factory).client('cloudtrail')
+        resp = client.describe_trails(trailNameList=[TRAIL])
+        trails = resp['trailList']
+        arn = trails[0]['TrailARN']
+        status = client.get_trail_status(Name=arn)
+        self.assertTrue(status['IsLogging'])
 
-
+    def test_enable_trail(self):
+        factory = self.replay_flight_data('test_cloudtrail_enable')
+        p = self.load_policy(
+            {
+                'name': 'trail-test',
+                'resource': 'account',
+                'actions': [
+                    {
+                        'type': 'enable-cloudtrail',
+                        'trail': TRAIL,
+                        'bucket': '%s-bucket' % TRAIL,
+                        'multi-region': False,
+                        'global-events': False,
+                        'notify': 'test',
+                        'file-digest': True,
+                        'kms': True,
+                        'kms-key': 'arn:aws:kms:us-east-1:1234:key/fake',
+                    },
+                ],
+            },
+            session_factory=factory,
+        )
+        p.run()
+        client = local_session(factory).client('cloudtrail')
+        resp = client.describe_trails(trailNameList=[TRAIL])
+        trails = resp['trailList']
+        test_trail = trails[0]
+        self.assertFalse(test_trail['IsMultiRegionTrail'])
+        self.assertFalse(test_trail['IncludeGlobalServiceEvents'])
+        self.assertTrue(test_trail['LogFileValidationEnabled'])
+        self.assertEqual(test_trail['SnsTopicName'], 'test')
+        arn = test_trail['TrailARN']
+        status = client.get_trail_status(Name=arn)
+        self.assertTrue(status['IsLogging'])
