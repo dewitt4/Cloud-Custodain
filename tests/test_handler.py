dewitt4/Cloud-Custodain

commit ebf82d892a2695d5697e0dfa9db78044c7b3b218

lambda policies - Allow setting execution options (cross account/region for periodic) (#1114)

c7n/handler.py
@@ -21,8 +21,10 @@ an event.
 import logging
 import os
 import uuid
+import json
 
-from c7n.policy import load
+from c7n.policy import PolicyCollection
+from c7n.resources import load_resources
 from c7n.utils import format_event, get_account_id_from_sts
 
 
@@ -80,7 +82,18 @@ def dispatch_event(event, context):
     if event['debug']:
         log.info("Processing event\n %s", format_event(event))
 
-    policies = load(Config.empty(), 'config.json', format='json')
+    # policies file should always be valid in lambda so do loading naively
+    with open('config.json') as f:
+        policy_config = json.load(f)
+
+    if not policy_config or not policy_config.get('policies'):
+        return True
+
+    options_overrides = policy_config['policies'][0]['mode'].get('execution-options', {})
+    options = Config.empty(**options_overrides)
+
+    load_resources()
+    policies = PolicyCollection(policy_config, options)
     if policies:
         for p in policies:
             p.push(event, context)

commit 0bc169921749cc010e7fd97e851db0647576b254

lambda policy - fix trunk only regression from #1114  (#1226)

c7n/handler.py
@@ -87,13 +87,15 @@ def dispatch_event(event, context):
         policy_config = json.load(f)
 
     if not policy_config or not policy_config.get('policies'):
-        return True
+        return False
 
-    options_overrides = policy_config['policies'][0]['mode'].get('execution-options', {})
+    # TODO. This enshrines an assumption of a single policy per lambda.
+    options_overrides = policy_config[
+        'policies'][0].get('mode', {}).get('execution-options', {})
     options = Config.empty(**options_overrides)
 
     load_resources()
-    policies = PolicyCollection(policy_config, options)
+    policies = PolicyCollection.from_data(policy_config, options)
     if policies:
         for p in policies:
             p.push(event, context)

tools/c7n_logexporter/c7n_logexporter/exporter.py
@@ -99,6 +99,7 @@ def cli():
 @cli.command()
 @click.option('--config', type=click.Path())
 def validate(config):
+    """validate config file."""
     with open(config) as fh:
         content = fh.read()
 
@@ -124,6 +125,7 @@ def validate(config):
 @click.option('--end')
 @debug
 def run(config, start, end):
+    """run export across accounts and log groups specified in config."""
     config = validate.callback(config)
     destination = config.get('destination')
     start = start and parse(start) or start
@@ -348,6 +350,7 @@ def filter_extant_exports(client, bucket, prefix, days, start, end=None):
 # @click.option('--stream-prefix)
 @lambdafan
 def export(group, bucket, prefix, start, end, role, session=None):
+    """Export a single log group to s3."""
     start = start and isinstance(start, basestring) and parse(start) or start
     end = (end and isinstance(start, basestring) and
            parse(end) or end or datetime.now())

tests/test_handler.py
@@ -18,6 +18,7 @@ import shutil
 import tempfile
 
 from common import BaseTest
+from c7n.policy import Policy
 
 
 class HandleTest(BaseTest):
@@ -39,10 +40,23 @@ class HandleTest(BaseTest):
         self.addCleanup(cleanup)
         self.change_environment(C7N_OUTPUT_DIR=self.run_dir)
 
+
+        policy_execution = []
+
+        def push(self, event, context):
+            policy_execution.append((event, context))
+
+        self.patch(Policy, 'push', push)
+            
         from c7n import handler
 
         with open(os.path.join(self.run_dir, 'config.json'), 'w') as fh:
-            json.dump({'policies': []}, fh)
+            json.dump(
+                {'policies': [
+                    {'resource': 'asg',
+                     'name': 'autoscaling',
+                     'filters': [],
+                     'actions': []}]}, fh)
 
         self.assertEqual(
             handler.dispatch_event(
@@ -50,6 +64,9 @@ class HandleTest(BaseTest):
             None)
         self.assertEqual(
             handler.dispatch_event({'detail': {}}, None), True)
+        self.assertEqual(
+            policy_execution,
+            [({'detail': {}, 'debug': True}, None)])
 
         config = handler.Config.empty()
         self.assertEqual(config.assume_role, None)