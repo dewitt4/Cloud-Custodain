218a14afcea472fc96d181e0361f28e284fad1c3
JohnTheodore <JohnTheodore42@gmail.com>
2017-05-31 19:21:33 -0700

    tests - simplify test_logsub and fix config file race (#1235)

--- tests/test_logsub.py
+++ tests/test_logsub.py
@@ -11,10 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import json
-import os
-import tempfile
-
 from unittest import TestCase
 from c7n.logs_support import _timestamp_from_string
 from c7n.ufuncs import logsub
@@ -23,22 +19,9 @@ from c7n.ufuncs import logsub
 class TestLogsub(TestCase):
 
     def setUp(self):
-        self.old_dir = os.getcwd()
-        os.chdir(tempfile.gettempdir())
-        self.config_data = {
+        logsub.config = {
             'test': 'data',
         }
-        with open('config.json', 'w') as conf:
-            json.dump(self.config_data, conf)
-
-    def tearDown(self):
-        if os.path.isfile('config.json'):
-            os.remove('config.json')
-        os.chdir(self.old_dir)
-
-    def test_init(self):
-        logsub.init()
-        self.assertEqual(logsub.config, self.config_data)
 
     def test_message_event(self):
         event = {
