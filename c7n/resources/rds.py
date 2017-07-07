50136817a53d8ad0857056fc5c64278893ff5723
taohungyang <proty.ece96@g2.nctu.edu.tw>
2017-07-07 16:13:56 -0400

    rds - param filter fix typo from "regions" to "region" (#1381)

--- c7n/resources/rds.py
+++ c7n/resources/rds.py
@@ -1568,7 +1568,7 @@ class ParameterFilter(ValueFilter):
 
         for pg in param_groups:
             cache_key = {
-                'region': self.manager.config.regions,
+                'region': self.manager.config.region,
                 'account_id': self.manager.config.account_id,
                 'rds-pg': pg}
             pg_values = self.manager._cache.get(cache_key)
