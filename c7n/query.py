40291a631fe2b0498735250be56ac86b5023a590
Raphael Deem <raphael.deem@gmail.com>
2017-08-03 20:09:06 -0700

    core - query get resources accept strings as resources  (#1399) (#1398)

--- c7n/query.py
+++ c7n/query.py
@@ -91,7 +91,12 @@ class ResourceQuery(object):
 
         resources = self.filter(resource_type, **params)
         if client_filter:
-            resources = [r for r in resources if r[m.id] in identities]
+            # This logic was added to prevent the issue from:
+            # https://github.com/capitalone/cloud-custodian/issues/1398
+            if all(map(lambda r: isinstance(r, six.string_types), resources)):
+                resources = [r for r in resources if r in identities]
+            else:
+                resources = [r for r in resources if r[m.id] in identities]
 
         return resources
 
