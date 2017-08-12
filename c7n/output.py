2492b1ed7fa1b8122d254252d5f3f160a79a7e78
Kapil Thangavelu <kapilt@gmail.com>
2017-08-12 08:58:32 -0400

    outputs - retry on cloudwatch metrics output (#1475)

--- c7n/output.py
+++ c7n/output.py
@@ -29,7 +29,7 @@ import tempfile
 import os
 
 from boto3.s3.transfer import S3Transfer
-from c7n.utils import local_session, parse_s3
+from c7n.utils import local_session, parse_s3, get_retry
 from c7n.log import CloudWatchLogHandler
 
 DEFAULT_NAMESPACE = "CloudMaid"
@@ -42,6 +42,8 @@ class MetricsOutput(object):
     """
 
     permissions = ("cloudWatch:PutMetricData",)
+
+    retry = get_retry(('Throttling',))
 
     @staticmethod
     def select(metrics_enabled):
@@ -81,7 +83,8 @@ class MetricsOutput(object):
 
     def _put_metrics(self, ns, metrics):
         watch = local_session(self.ctx.session_factory).client('cloudwatch')
-        return watch.put_metric_data(Namespace=ns, MetricData=metrics)
+        return self.retry(
+            watch.put_metric_data, Namespace=ns, MetricData=metrics)
 
 
 class NullMetricsOutput(MetricsOutput):
