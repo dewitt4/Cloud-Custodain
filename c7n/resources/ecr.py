161e7f7760a037c2d30a06dbbe7c10dd685cb5bc
DhruvSalwan <salwandhruvdev@gmail.com>
2017-09-06 06:29:11 -0400

    resources/ecr remove-statement action (#1537)

--- c7n/resources/ecr.py
+++ c7n/resources/ecr.py
@@ -15,10 +15,12 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 from botocore.exceptions import ClientError
 
+import json
 from c7n.filters import CrossAccountAccessFilter
 from c7n.manager import resources
 from c7n.query import QueryResourceManager
-from c7n.utils import local_session
+from c7n.actions import BaseAction
+from c7n.utils import local_session, type_schema
 
 
 @resources.register('ecr')
@@ -30,6 +32,9 @@ class ECR(QueryResourceManager):
         name = "repositoryName"
         id = "repositoryArn"
         dimension = None
+
+
+ErrPolicyNotFound = 'RepositoryPolicyNotFoundException'
 
 
 @ECR.filter_registry.register('cross-account')
@@ -59,7 +64,7 @@ class ECRCrossAccountAccessFilter(CrossAccountAccessFilter):
                 r['Policy'] = client.get_repository_policy(
                     repositoryName=r['repositoryName'])['policyText']
             except ClientError as e:
-                if e.response['Error']['Code'] == 'RepositoryPolicyNotFoundException':
+                if e.response['Error']['Code'] == ErrPolicyNotFound:
                     return None
                 raise
             return r
@@ -68,4 +73,93 @@ class ECRCrossAccountAccessFilter(CrossAccountAccessFilter):
         with self.executor_factory(max_workers=3) as w:
             resources = list(filter(None, w.map(_augment, resources)))
 
-        return super(ECRCrossAccountAccessFilter, self).process(resources, event)
+        return super(ECRCrossAccountAccessFilter, self).process(
+            resources, event)
+
+
+@ECR.action_registry.register('remove-statements')
+class RemovePolicyStatement(BaseAction):
+    """Action to remove policy statements from ECR
+
+    :example:
+
+        .. code-block: yaml
+
+            policies:
+              - name: ecr-remove-cross-accounts
+                resource: s3
+                filters:
+                  - type: cross-account
+                actions:
+                  - type: remove-statements
+                    statement_ids: matched
+    """
+
+    schema = type_schema(
+        'remove-statements',
+        required=['statement_ids'],
+        statement_ids={'oneOf': [
+            {'enum': ['matched']},
+            {'type': 'array', 'items': {'type': 'string'}}]})
+    permissions = ("ecr:SetRepositoryPolicy",)
+
+    def process(self, resources):
+        results = []
+        client = local_session(self.manager.session_factory).client('ecr')
+        for r in resources:
+            try:
+                if self.process_resource(client, r):
+                    results.append(r)
+            except:
+                self.log.exception(
+                    "Error processing ecr registry:%s", r['repositoryArn'])
+        return results
+
+    def process_resource(self, client, resource):
+        if 'Policy' not in resource:
+            try:
+                resource['Policy'] = client.get_repository_policy(
+                    repositoryName=resource['repositoryName'])['policyText']
+            except ClientError as e:
+                if e.response['Error']['Code'] != ErrPolicyNotFound:
+                    raise
+                resource['Policy'] = None
+
+        if not resource['Policy']:
+            return
+
+        p = json.loads(resource['Policy'])
+        statements, found = self.process_policy(p, resource)
+
+        if statements is None:
+            return
+        if not statements:
+            client.delete_repository_policy(
+                repositoryName=resource['repositoryName'])
+        else:
+            client.set_repository_policy(
+                repositoryName=resource['repositoryName'],
+                policyText=json.dumps(p))
+        return {'Name': resource['repositoryName'],
+                'State': 'PolicyRemoved',
+                'Statements': found}
+
+    def process_policy(self, policy, resource):
+        statement_ids = self.data.get('statement_ids')
+
+        found = []
+        statements = policy.get('Statement', [])
+        resource_statements = resource.get(
+            CrossAccountAccessFilter.annotation_key, ())
+
+        for s in list(statements):
+            if statement_ids == ['matched']:
+                if s in resource_statements:
+                    found.append(s)
+                    statements.remove(s)
+            elif s['Sid'] in self.data['statement_ids']:
+                found.append(s)
+                statements.remove(s)
+        if not found:
+            return None, found
+        return statements, found
