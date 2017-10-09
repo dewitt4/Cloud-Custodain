a83b3dc916b3d2ed457604403295c65eaa26b41e
Kapil Thangavelu <kapilt@gmail.com>
2017-10-09 07:52:20 -0600

    version increment (0.8.27.0) (#1637)

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setup(
     name="c7n",
-    version='0.8.26.1',
+    version='0.8.27.0',
     description="Cloud Custodian - Policy Rules Engine",
     long_description=read('README.rst'),
     classifiers=[
