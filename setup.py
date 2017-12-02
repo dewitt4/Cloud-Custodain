3ee11a84c5dc37e8aff2095a12a6d1dbbb91d907
Calvin Hendryx-Parker <calvin@sixfeetup.com>
2017-12-01 19:56:56 -0700

    setup.py utf8 encode README fixes #1853 (#1862)

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 import os
+from io import open
 from setuptools import setup, find_packages
 
 
 def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    return open(os.path.join(os.path.dirname(__file__), fname), encoding='utf-8').read()
 
 
 setup(
