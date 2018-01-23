917acc6cb93f0cf950b8d7e29404fd3fad154b09
LykinsN <LykinsN2015@gmail.com>
2018-01-23 11:26:33 -0500

    dynamodb backup support - creation and deletion of backups. (#1942)

--- c7n/resources/dynamodb.py
+++ c7n/resources/dynamodb.py
@@ -22,7 +22,7 @@ from c7n import query
 from c7n.manager import resources
 from c7n.tags import TagDelayedAction, RemoveTag, TagActionFilter, Tag
 from c7n.utils import (
-    local_session, get_retry, chunks, type_schema)
+    local_session, get_retry, chunks, type_schema, snapshot_identifier)
 
 
 filters = FilterRegistry('dynamodb-table.filters')
@@ -100,6 +100,14 @@ class StatusFilter(object):
         states = states or self.valid_states
         orig_count = len(tables)
         result = [t for t in tables if t['TableStatus'] in states]
+        self.log.info("%s %d of %d tables" % (
+            self.__class__.__name__, len(result), orig_count))
+        return result
+
+    def filter_backup_state(self, tables, states=None):
+        states = states or self.valid_states
+        orig_count = len(tables)
+        result = [t for t in tables if t['BackupStatus'] in states]
         self.log.info("%s %d of %d tables" % (
             self.__class__.__name__, len(result), orig_count))
         return result
@@ -229,15 +237,129 @@ class DeleteTable(BaseAction, StatusFilter):
         if not len(resources):
             return
 
-        for table_set in chunks(resources, 20):
-            with self.executor_factory(max_workers=3) as w:
-                futures = []
+        futures = []
+
+        with self.executor_factory(max_workers=2) as w:
+            for table_set in chunks(resources, 20):
                 futures.append(w.submit(self.delete_table, table_set))
-                for f in as_completed(futures):
-                    if f.exception():
-                        self.log.error(
-                            "Exception deleting dynamodb table set \n %s" % (
-                                f.exception()))
+            for f in as_completed(futures):
+                if f.exception():
+                    self.log.error(
+                        "Exception deleting dynamodb table set \n %s"
+                        % (f.exception()))
+
+
+@Table.action_registry.register('backup')
+class CreateBackup(BaseAction, StatusFilter):
+    """Creates a manual backup of a DynamoDB table. Use of the optional
+       prefix flag will attach a user specified prefix. Otherwise,
+       the backup prefix will default to 'Backup'.
+
+    :example:
+
+    .. code-block:: yaml
+
+            policies:
+              - name: dynamodb-create-backup
+                resource: dynamodb-table
+                actions:
+                  - type: backup
+                    prefix: custom
+    """
+
+    valid_status = ('ACTIVE',)
+    schema = type_schema('backup',
+                prefix={'type': 'string'})
+    permissions = ('dynamodb:CreateBackup',)
+
+    def process(self, resources):
+        resources = self.filter_table_state(
+            resources, self.valid_status)
+        if not len(resources):
+            return
+
+        c = local_session(self.manager.session_factory).client('dynamodb')
+        futures = {}
+
+        prefix = self.data.get('prefix', 'Backup')
+
+        with self.executor_factory(max_workers=2) as w:
+            for t in resources:
+                futures[w.submit(
+                    c.create_backup,
+                    BackupName=snapshot_identifier(
+                        prefix, t['TableName']),
+                    TableName=t['TableName'])] = t
+            for f in as_completed(futures):
+                t = futures[f]
+                if f.exception():
+                    self.manager.log.warning(
+                        "Could not complete DynamoDB backup table:%s", t)
+                arn = f.result()['BackupDetails']['BackupArn']
+                t['c7n:BackupArn'] = arn
+
+
+@resources.register('dynamodb-backup')
+class Backup(query.QueryResourceManager):
+
+    class resource_type(object):
+        service = 'dynamodb'
+        type = 'table'
+        enum_spec = ('list_backups', 'BackupSummaries', None)
+        detail_spec = None
+        id = 'Table'
+        filter_name = None
+        name = 'TableName'
+        date = 'BackupCreationDateTime'
+        dimension = 'TableName'
+        config_type = 'AWS::DynamoDB::Table'
+
+
+@Backup.action_registry.register('delete')
+class DeleteBackup(BaseAction, StatusFilter):
+    """Deletes backups of a DynamoDB table
+
+    :example:
+
+    .. code-block:: yaml
+
+            policies:
+              - name: dynamodb-delete-backup
+                resource: dynamodb-backup
+                filters:
+                  - type: age
+                    days: 28
+                    op: ge
+                actions:
+                  - type: delete
+    """
+
+    valid_status = ('AVAILABLE',)
+    schema = type_schema('delete')
+    permissions = ('dynamodb:DeleteBackup',)
+
+    def process(self, backups):
+        backups = self.filter_backup_state(
+            backups, self.valid_status)
+        if not len(backups):
+            return
+
+        c = local_session(self.manager.session_factory).client('dynamodb')
+
+        for table_set in chunks(backups, 20):
+            self.process_dynamodb_backups(table_set, c)
+
+    def process_dynamodb_backups(self, table_set, c):
+
+        for t in table_set:
+            try:
+                c.delete_backup(
+                    BackupArn=t['BackupArn'])
+            except ClientError as e:
+                if e.response['Error']['Code'] == 'ResourceNotFoundException':
+                    self.log.warning("Could not complete DynamoDB backup table:%s", t)
+                    continue
+                raise
 
 
 @resources.register('dynamodb-stream')
--- tests/test_dynamodb.py
+++ tests/test_dynamodb.py
@@ -14,6 +14,7 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 from .common import BaseTest
+import datetime
 
 from c7n.resources.dynamodb import DeleteTable
 from c7n.executor import MainThreadExecutor
@@ -135,3 +136,67 @@ class DynamodbTest(BaseTest):
         self.assertEqual(len(resources), 1)
         tags = client.list_tags_of_resource(ResourceArn=arn)
         self.assertFalse('test_key' in tags)
+
+    def test_dynamodb_create_backup(self):
+        dt = datetime.datetime.now().replace(
+            year=2018, month=1, day=16, hour=19, minute=39)
+        suffix = dt.strftime('%Y-%m-%d-%H-%M')
+
+        session_factory = self.replay_flight_data(
+            'test_dynamodb_create_backup')
+
+        p = self.load_policy({
+                'name': 'c7n-dynamodb-create-backup',
+                'resource': 'dynamodb-table',
+                'filters': [{'TableName': 'c7n-dynamodb-backup'}],
+                'actions': [{
+                    'type': 'backup'}]
+            },
+            session_factory=session_factory)
+        resources = p.run()
+        self.assertEqual(len(resources), 1)
+
+        client = session_factory().client('dynamodb')
+        arn = resources[0]['c7n:BackupArn']
+        table = client.describe_backup(
+            BackupArn=arn)
+        self.assertEqual(table['BackupDescription']['BackupDetails']['BackupName'],
+            'Backup-c7n-dynamodb-backup-%s' % (suffix))
+
+    def test_dynamodb_create_prefixed_backup(self):
+        dt = datetime.datetime.now().replace(
+            year=2018, month=1, day=22, hour=13, minute=42)
+        suffix = dt.strftime('%Y-%m-%d-%H-%M')
+
+        session_factory = self.replay_flight_data(
+            'test_dynamodb_create_prefixed_backup')
+
+        p = self.load_policy({
+            'name': 'c7n-dynamodb-create-prefixed-backup',
+            'resource': 'dynamodb-table',
+            'filters': [{'TableName': 'c7n-dynamodb-backup'}],
+            'actions': [{
+                'type': 'backup',
+                'prefix': 'custom'}]
+        },
+            session_factory=session_factory)
+        resources = p.run()
+        self.assertEqual(len(resources), 1)
+
+        client = session_factory().client('dynamodb')
+        arn = resources[0]['c7n:BackupArn']
+        table = client.describe_backup(
+            BackupArn=arn)
+        self.assertEqual(table['BackupDescription']['BackupDetails']['BackupName'],
+                         'custom-c7n-dynamodb-backup-%s' % (suffix))
+
+    def test_dynamodb_delete_backup(self):
+        factory = self.replay_flight_data('test_dynamodb_delete_backup')
+        p = self.load_policy({
+            'name': 'c7n-dynamodb-delete-backup',
+            'resource': 'dynamodb-backup',
+            'filters': [{'TableName': 'c7n-dynamodb-backup'}],
+            'actions': ['delete']},
+            session_factory=factory)
+        resources = p.run()
+        self.assertEqual(len(resources), 1)
