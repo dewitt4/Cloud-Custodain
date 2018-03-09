b65f963ee5822b626fd112d22eb433c169ba22ed
Kapil Thangavelu <kapilt@gmail.com>
2018-03-09 02:36:46 -0500

    tool for log processing and metrics collection (#2082)

--- tools/dev/license-headers.py
+++ tools/dev/license-headers.py
@@ -1,4 +1,4 @@
-# Copyright 2016-2017 Capital One Services, LLC
+# Copyright 2016-2018 Capital One Services, LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -21,7 +21,7 @@ import sys
 import c7n
 
 header = """\
-# Copyright 2017 Capital One Services, LLC
+# Copyright 2017-2018 Capital One Services, LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
