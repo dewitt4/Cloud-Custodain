2d32754611ef1b7ccd774467f316878daa74d7f3
getglad <getglad@users.noreply.github.com>
2018-03-23 14:35:12 -0400

    sns - cross-access filter validation fixes (#2152)

--- c7n/actions.py
+++ c7n/actions.py
@@ -865,11 +865,14 @@ class ModifyPolicyBase(BaseAction):
     )
 
     def __init__(self, data=None, manager=None):
-        config_args = {
-            'account_id': manager.config.account_id,
-            'region': manager.config.region
-        }
-        self.data = utils.format_string_values(data, **config_args)
+        if manager is not None:
+            config_args = {
+                'account_id': manager.config.get('account_id'),
+                'region': manager.config.get('region')
+            }
+            self.data = utils.format_string_values(data, **config_args)
+        else:
+            self.data = utils.format_string_values(data)
         self.manager = manager
 
     def add_statements(self, policy_statements):
