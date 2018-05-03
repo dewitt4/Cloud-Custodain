237e842fe963d42df6d07856ae6dd72b038978d2
Kapil Thangavelu <kapilt@gmail.com>
2018-05-03 13:17:16 -0400

    core - blob output directory storage - strip file protocol

--- c7n/output.py
+++ c7n/output.py
@@ -206,6 +206,8 @@ class DirectoryOutput(FSOutput):
 
     def __init__(self, ctx):
         super(DirectoryOutput, self).__init__(ctx)
+        if self.root_dir.startswith('file://'):
+            self.root_dir = self.root_dir[len('file://'):]
         if self.ctx.output_path is not None:
             if not os.path.exists(self.ctx.output_path):
                 os.makedirs(self.ctx.output_path)
