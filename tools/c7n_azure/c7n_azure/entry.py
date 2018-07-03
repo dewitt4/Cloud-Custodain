d740042b012037d71d4612c3dd69cf1329cde4b5
Tanner Barlow <tanner.barlow12@gmail.com>
2018-07-03 07:53:22 -0700

    azure - add new resources (#2593)

--- tools/c7n_azure/c7n_azure/entry.py
+++ tools/c7n_azure/c7n_azure/entry.py
@@ -30,6 +30,10 @@ import c7n_azure.resources.network_security_group
 import c7n_azure.resources.web_app
 import c7n_azure.resources.access_control
 import c7n_azure.resources.network_interface
+import c7n_azure.resources.disk
+import c7n_azure.resources.cognitive_service
+import c7n_azure.resources.data_factory
+import c7n_azure.resources.iot_hub
 import c7n_azure.resources.appserviceplan  # noqa: F401
 
 
