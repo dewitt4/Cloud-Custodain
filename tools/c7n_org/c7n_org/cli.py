2a8adc5ca378f5d2700b98d1b8ce7347a3bcbf22
Kapil Thangavelu <kapilt@gmail.com>
2018-10-20 15:05:34 -0400

    tools/c7n-org - exit 1 if failure for any policy execution after running all policies (#2659)

--- tools/c7n_org/c7n_org/cli.py
+++ tools/c7n_org/c7n_org/cli.py
@@ -465,7 +465,9 @@ def run_account(account, region, policies_config, output_path,
 
     policies = PolicyCollection.from_data(policies_config, config)
     policy_counts = {}
+    success = True
     st = time.time()
+
     with environ(**account_tags(account)):
         for p in policies:
 
@@ -486,15 +488,17 @@ def run_account(account, region, policies_config, output_path,
                     account['name'], region, p.name, len(resources),
                     time.time() - st)
             except ClientError as e:
+                success = False
                 if e.response['Error']['Code'] == 'AccessDenied':
                     log.warning('Access denied account:%s region:%s',
                                 account['name'], region)
-                    return policy_counts
+                    return policy_counts, success
                 log.error(
                     "Exception running policy:%s account:%s region:%s error:%s",
                     p.name, account['name'], region, e)
                 continue
             except Exception as e:
+                success = False
                 log.error(
                     "Exception running policy:%s account:%s region:%s error:%s",
                     p.name, account['name'], region, e)
@@ -505,7 +509,7 @@ def run_account(account, region, policies_config, output_path,
                 pdb.post_mortem(sys.exc_info()[-1])
                 raise
 
-    return policy_counts
+    return policy_counts, success
 
 
 @cli.command(name='run')
@@ -529,6 +533,7 @@ def run(config, use, output_dir, accounts, tags,
     accounts_config, custodian_config, executor = init(
         config, use, debug, verbose, accounts, tags, policy, policy_tags=policy_tags)
     policy_counts = Counter()
+    success = True
     with executor(max_workers=WORKER_COUNT) as w:
         futures = {}
         for a in accounts_config['accounts']:
@@ -552,7 +557,14 @@ def run(config, use, output_dir, accounts, tags,
                     "Error running policy in %s @ %s exception: %s",
                     a['name'], r, f.exception())
 
-            for p, count in f.result().items():
+            account_region_pcounts, account_region_success = f.result()
+            for p, count in account_region_pcounts:
                 policy_counts[p] += count
 
+            if not account_region_success:
+                success = False
+
     log.info("Policy resource counts %s" % policy_counts)
+
+    if not success:
+        sys.exit(1)
