6d614559f63705ded2e7be49b1d1200da4c9f7ea
Kapil Thangavelu <kapilt@gmail.com>
2018-11-05 09:22:13 -0500

    gcp.subnet - flow log and private api endpoint actions (#3101)

--- tools/c7n_gcp/c7n_gcp/resources/network.py
+++ tools/c7n_gcp/c7n_gcp/resources/network.py
@@ -11,9 +11,13 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import re
+
+from c7n_gcp.actions import MethodAction
 from c7n_gcp.query import QueryResourceManager, TypeInfo
 
 from c7n_gcp.provider import resources
+from c7n.utils import type_schema
 
 
 @resources.register('vpc')
@@ -32,9 +36,72 @@ class Subnet(QueryResourceManager):
     class resource_type(TypeInfo):
         service = 'compute'
         version = 'v1'
-        component = 'networks'
+        component = 'subnetworks'
         enum_spec = ('aggregatedList', 'items.*.subnetworks[]', None)
-        scope_template = "projects/{}/aggregated/subnetworks"
+
+        @staticmethod
+        def get(client, resource_info):
+            return client.execute_query(
+                'get', {'project': resource_info['project_id'],
+                        'region': resource_info['location'],
+                        'subnetwork': resource_info['subnetwork_name']})
+
+
+class SubnetAction(MethodAction):
+
+    path_param_re = re.compile(
+        '.*?/projects/(.*?)/regions/(.*?)/subnetworks/(.*)')
+
+    def get_resource_params(self, model, resource):
+        project, region, subnet = self.path_param_re.match(
+            resource['selfLink']).groups()
+        return {'project': project, 'region': region, 'subnetwork': subnet}
+
+
+@Subnet.action_registry.register('set-flow-log')
+class SetFlowLog(SubnetAction):
+    """Enable vpc flow logs on a subnet.
+
+    :example: Enable flow logs on all subnets
+
+    .. yaml:
+
+     policies:
+       - name: flow-active
+         resource: gcp.subnet
+         filters:
+          - enableFlowLogs: empty
+         actions:
+          - set-flow-log
+
+    """
+
+    schema = type_schema(
+        'set-flow-log',
+        state={'type': 'boolean', 'default': True})
+    method_spec = {'op': 'patch'}
+
+    def get_resource_params(self, m, r):
+        params = super(SetFlowLog, self).get_resource_params(m, r)
+        params['body'] = dict(r)
+        params['body']['enableFlowLogs'] = self.data.get('state', True)
+        return params
+
+
+@Subnet.action_registry.register('set-private-api')
+class SetGcpPrivateAccess(SubnetAction):
+    """Enable/Disable GCP Private IP Access for a subnet"""
+
+    schema = type_schema(
+        'set-gcp-private',
+        state={'type': 'boolean', 'default': True})
+    method_spec = {'op': 'setPrivateIpGoogleAccess'}
+
+    def get_resource_params(self, m, r):
+        params = super(SetGcpPrivateAccess, self).get_resource_params(m, r)
+        params['body'] = {
+            'privateIpGoogleAccess': self.data.get('state', True)}
+        return params
 
 
 @resources.register('firewall')
@@ -44,6 +111,12 @@ class Firewall(QueryResourceManager):
         service = 'compute'
         version = 'v1'
         component = 'firewalls'
+
+        @staticmethod
+        def get(client, resource_info):
+            return client.execute_query(
+                'get', {'project': resource_info['project_id'],
+                        'firewall': resource_info['resourceName'].rsplit('/', 1)[-1]})
 
 
 @resources.register('router')
---
+++ tools/c7n_gcp/tests/test_network.py
@@ -0,0 +1,78 @@
+
+from gcp_common import BaseTest
+
+
+class FirewallTest(BaseTest):
+
+    def test_firewall_get(self):
+        factory = self.replay_flight_data(
+            'firewall-get', project_id='cloud-custodian')
+        p = self.load_policy({'name': 'fw', 'resource': 'gcp.firewall'},
+                             session_factory=factory)
+        fw = p.resource_manager.get_resource({
+            'resourceName': 'projects/cloud-custodian/global/firewalls/allow-inbound-xyz',
+            'firewall_rule_id': '4746899906201084445',
+            'project_id': 'cloud-custodian'})
+        self.assertEqual(fw['name'], 'allow-inbound-xyz')
+
+
+class SubnetTest(BaseTest):
+
+    def test_subnet_get(self):
+        factory = self.replay_flight_data(
+            'subnet-get-resource', project_id='cloud-custodian')
+        p = self.load_policy({'name': 'subnet', 'resource': 'gcp.subnet'},
+                             session_factory=factory)
+        subnet = p.resource_manager.get_resource({
+            "location": "us-central1",
+            "project_id": "cloud-custodian",
+            "subnetwork_id": "4686700484947109325",
+            "subnetwork_name": "default"})
+        self.assertEqual(subnet['name'], 'default')
+        self.assertEqual(subnet['privateIpGoogleAccess'], True)
+
+    def test_subnet_set_flow(self):
+        project_id = 'cloud-custodian'
+        factory = self.replay_flight_data('subnet-set-flow', project_id=project_id)
+        p = self.load_policy({
+            'name': 'all-subnets',
+            'resource': 'gcp.subnet',
+            'filters': [
+                {"id": "4686700484947109325"},
+                {"enableFlowLogs": "empty"}],
+            'actions': ['set-flow-log']}, session_factory=factory)
+        resources = p.run()
+
+        self.assertEqual(len(resources), 1)
+        subnet = resources.pop()
+        self.assertEqual(subnet['enableFlowLogs'], False)
+
+        client = p.resource_manager.get_client()
+        result = client.execute_query(
+            'get', {'project': project_id,
+                    'region': 'us-central1',
+                    'subnetwork': subnet['name']})
+        self.assertEqual(result['enableFlowLogs'], True)
+
+    def test_subnet_set_private_api(self):
+        project_id = 'cloud-custodian'
+        factory = self.replay_flight_data('subnet-set-private-api', project_id=project_id)
+        p = self.load_policy({
+            'name': 'one-subnet',
+            'resource': 'gcp.subnet',
+            'filters': [
+                {"id": "4686700484947109325"},
+                {"privateIpGoogleAccess": False}],
+            'actions': ['set-private-api']}, session_factory=factory)
+        resources = p.run()
+
+        self.assertEqual(len(resources), 1)
+        subnet = resources.pop()
+        self.assertEqual(subnet['privateIpGoogleAccess'], False)
+
+        client = p.resource_manager.get_client()
+        result = client.execute_query(
+            'get', {'project': project_id,
+                    'region': 'us-central1',
+                    'subnetwork': subnet['name']})
+        self.assertEqual(result['privateIpGoogleAccess'], True)
