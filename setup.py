ef7ecf3430a90b8f9fb6590a7591bede10a996ba
David Filiatrault <FireballDWF@users.noreply.github.com>
2018-12-10 21:18:00 +0100

    aws - update boto sdk deps to support latest service releases (#3224)

--- setup.py
+++ setup.py
@@ -23,8 +23,8 @@ setup(
         'console_scripts': [
             'custodian = c7n.cli:main']},
     install_requires=[
-        "boto3>=1.9.54",
-        "botocore>=1.12.57",
+        "boto3>=1.9.62",
+        "botocore>=1.12.62",
         "python-dateutil>=2.6,<3.0.0",
         "pyyaml",
         "jsonschema",
