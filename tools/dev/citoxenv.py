49d2db2db642d497c5298d152c68908ba45cef3f
Kapil Thangavelu <kapilt@gmail.com>
2018-12-16 21:42:21 -0500

    ci - code coverage from azure pipeline (#3250)

--- tools/dev/citoxenv.py
+++ tools/dev/citoxenv.py
@@ -2,7 +2,7 @@
 import os
 pyver = os.environ.get('TRAVIS_PYTHON_VERSION', '')
 if pyver == '2.7':
-    print('py27-cov')
+    print('py27')
 elif pyver == '3.6':
     print('py36')
 elif pyver == '3.7':
