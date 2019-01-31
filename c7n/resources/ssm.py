b0d6c9572e00315e1a0c991f7235f51d038ea9dd
Kapil Thangavelu <kapilt@gmail.com>
2019-01-31 09:02:34 -0500

    aws - ssm send command (#3473)

--- c7n/resources/ssm.py
+++ c7n/resources/ssm.py
@@ -14,10 +14,14 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 
+from c7n.exceptions import PolicyValidationError
 from c7n.query import QueryResourceManager
 from c7n.manager import resources
-from c7n.utils import get_retry, local_session, type_schema
+from c7n.utils import chunks, get_retry, local_session, type_schema
 from c7n.actions import Action
+
+from .aws import shape_validate
+from .ec2 import EC2
 
 
 @resources.register('ssm-parameter')
@@ -47,6 +51,80 @@ class ManagedInstance(QueryResourceManager):
         dimension = None
         filter_name = None
     permissions = ('ssm:DescribeInstanceInformation',)
+
+
+@EC2.action_registry.register('send-command')
+@ManagedInstance.action_registry.register('send-command')
+class SendCommand(Action):
+    """Run an SSM Automation Document on an instance.
+
+    :Example:
+
+    Find ubuntu 18.04 instances are active with ssm.
+
+    .. code-block:: yaml
+
+        policies:
+          - name: ec2-osquery-install
+            resource: ec2
+            filters:
+              - type: ssm
+                key: PingStatus
+                value: Online
+              - type: ssm
+                key: PlatformName
+                value: Ubuntu
+              - type: ssm
+                key: PlatformVersion
+                value: 18.04
+            actions:
+              - type: send-command
+                command:
+                  DocumentName: AWS-RunShellScript
+                  Parameters:
+                    commands:
+                      - wget https://pkg.osquery.io/deb/osquery_3.3.0_1.linux.amd64.deb
+                      - dpkg -i osquery_3.3.0_1.linux.amd64.deb
+    """
+
+    schema = type_schema(
+        'send-command',
+        command={'type': 'object'},
+        required=('command',))
+
+    permissions = ('ssm:SendCommand',)
+    shape = "SendCommandRequest"
+    annotation = 'c7n:SendCommand'
+
+    def validate(self):
+        shape_validate(self.data['command'], self.shape, 'ssm')
+        # If used against an ec2 resource, require an ssm status filter
+        # to ensure that we're not trying to send commands to instances
+        # that aren't in ssm.
+        if self.manager.type != 'ec2':
+            return
+
+        found = False
+        for f in self.manager.iter_filters():
+            if f.type == 'ssm':
+                found = True
+                break
+        if not found:
+            raise PolicyValidationError(
+                "send-command requires use of ssm filter on ec2 resources")
+
+    def process(self, resources):
+        client = local_session(self.manager.session_factory).client('ssm')
+        for resource_set in chunks(resources, 50):
+            self.process_resource_set(client, resource_set)
+
+    def process_resource_set(self, client, resources):
+        command = dict(self.data['command'])
+        command['InstanceIds'] = [
+            r['InstanceId'] for r in resources]
+        result = client.send_command(**command).get('Command')
+        for r in resources:
+            r.setdefault('c7n:SendCommand', []).append(result['CommandId'])
 
 
 @resources.register('ssm-activation')
--- tests/test_ssm.py
+++ tests/test_ssm.py
@@ -13,10 +13,52 @@
 # limitations under the License.
 from __future__ import absolute_import, division, print_function, unicode_literals
 
+from c7n.exceptions import PolicyValidationError
 from .common import BaseTest, functional
+
+import time
 
 
 class TestSsm(BaseTest):
+
+    def test_ec2_ssm_send_command_validate(self):
+        self.assertRaises(
+            PolicyValidationError,
+            self.load_policy,
+            {'name': 'ssm-instances',
+             'resource': 'aws.ec2',
+             'actions': [
+                 {'type': 'send-command',
+                  'command': {
+                      'DocumentName': 'AWS-RunShellScript'}}]},
+            validate=True)
+
+    def test_ssm_send_command(self):
+        factory = self.replay_flight_data('test_ssm_send_command')
+        p = self.load_policy({
+            'name': 'ssm-instances',
+            'resource': 'ssm-managed-instance',
+            'filters': [{"PingStatus": "Online"}],
+            'actions': [
+                {'type': 'send-command',
+                 'command': {
+                     'DocumentName': 'AWS-RunShellScript',
+                     'Parameters': {
+                         'commands': [
+                             'wget https://pkg.osquery.io/deb/osquery_3.3.0_1.linux.amd64.deb',
+                             'dpkg -i osquery_3.3.0_1.linux.amd64.deb']}}}]},
+            session_factory=factory, config={'region': 'us-east-2'})
+        resources = p.run()
+        self.assertEqual(len(resources), 1)
+        self.assertTrue('c7n:SendCommand' in resources[0])
+
+        if self.recording:
+            time.sleep(5)
+
+        result = factory().client('ssm').get_command_invocation(
+            InstanceId=resources[0]['InstanceId'],
+            CommandId=resources[0]['c7n:SendCommand'][0])
+        self.assertEqual(result['Status'], 'Success')
 
     @functional
     def test_ssm_parameter_not_secure(self):
