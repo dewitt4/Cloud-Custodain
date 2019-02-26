b5af4d66fd303579250214082d7ed3e7f4f5ab79
joh08266 <43418865+joh08266@users.noreply.github.com>
2019-02-26 05:05:37 -0600

    aws - service-limit-increase add kinesis to service code mapping (#3578)

--- c7n/resources/account.py
+++ c7n/resources/account.py
@@ -600,6 +600,7 @@ class RequestLimitIncrease(BaseAction):
         'VPC': 'amazon-virtual-private-cloud',
         'IAM': 'aws-identity-and-access-management',
         'CloudFormation': 'aws-cloudformation',
+        'Kinesis': 'amazon-kinesis',
     }
 
     def process(self, resources):
