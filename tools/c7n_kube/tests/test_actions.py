b4c2620fd9c82fcaeee4f41fd49f6347c64b7a51
Sonny <theli@outlook.com>
2019-04-01 18:11:54 -0400

    k8s - backfill on patch action test (#3756)

--- tools/c7n_kube/tests/test_actions.py
+++ tools/c7n_kube/tests/test_actions.py
@@ -59,3 +59,35 @@ class TestDeleteAction(KubeTest):
         namespaces = client.list_service_for_all_namespaces().to_dict()['items']
         hello_node_service = [n for n in namespaces if n['metadata']['name'] == 'hello-node']
         self.assertFalse(hello_node_service)
+
+
+class TestPatchAction(KubeTest):
+    def test_patch_action(self):
+        factory = self.replay_flight_data()
+        p = self.load_policy(
+            {
+                'name': 'test-patch',
+                'resource': 'k8s.deployment',
+                'filters': [
+                    {'metadata.name': 'hello-node'},
+                    {'spec.replicas': 1}
+                ],
+                'actions': [
+                    {
+                        'type': 'patch',
+                        'options': {
+                            'spec': {
+                                'replicas': 2
+                            }
+                        }
+                    }
+                ]
+            },
+            session_factory=factory
+        )
+        resources = p.run()
+        self.assertTrue(len(resources), 1)
+        client = factory().client('Apps', 'V1')
+        deployments = client.list_deployment_for_all_namespaces().to_dict()['items']
+        hello_node_deployment = [d for d in deployments if d['metadata']['name'] == 'hello-node'][0]
+        self.assertEqual(hello_node_deployment['spec']['replicas'], 2)
