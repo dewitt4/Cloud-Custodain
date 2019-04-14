9af4fe8e1bb83b525c12ddb2c3d83c0055e87c23
AJ Kerrigan <akerrigan@mdsol.com>
2019-04-14 18:30:49 -0400

    aws.workspaces - connection status filter (#3793)

--- tests/test_workspaces.py
+++ tests/test_workspaces.py
@@ -13,9 +13,15 @@
 # limitations under the License.
 from __future__ import absolute_import, division, print_function, unicode_literals
 
+import datetime
+from dateutil import parser
+
 from .common import BaseTest
+from c7n import filters
 from c7n.executor import MainThreadExecutor
 from c7n.resources.workspaces import Workspace
+from c7n.testing import mock_datetime_now
+from c7n.utils import annotation
 
 
 class WorkspacesTest(BaseTest):
@@ -47,3 +53,24 @@ class WorkspacesTest(BaseTest):
         )
         resources = p.run()
         self.assertEqual(len(resources), 2)
+
+    def test_connection_status_filter(self):
+        session_factory = self.replay_flight_data("test_workspaces_connection_status")
+        p = self.load_policy(
+            {
+                "name": "workspaces-connection-status",
+                "resource": "workspaces",
+                "filters": [{
+                    "type": "connection-status",
+                    "value_type": "age",
+                    "key": "LastKnownUserConnectionTimestamp",
+                    "op": "ge",
+                    "value": 30
+                }]
+            }, session_factory=session_factory
+        )
+        with mock_datetime_now(parser.parse("2019-04-13T00:00:00+00:00"), datetime):
+            resources = p.run()
+        self.assertEqual(len(resources), 1)
+        self.assertIn('LastKnownUserConnectionTimestamp',
+            annotation(resources[0], filters.ANNOTATION_KEY))
