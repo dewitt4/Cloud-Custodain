be0dbc232257c7484a0ddc6a35320a211bddfea8
Kapil Thangavelu <kapilt@gmail.com>
2019-05-07 06:01:36 -0400

    tools/c7n-org - work around python osx multiprocessing bug (#3934)

--- tools/c7n_org/c7n_org/cli.py
+++ tools/c7n_org/c7n_org/cli.py
@@ -22,11 +22,6 @@ import subprocess
 import six
 import sys
 
-# Try to set this early if offers any help against this OSX issue
-# https://bugs.python.org/issue33725
-# if sys.platform == 'darwin':
-#    os.environ['OBJC_DISABLE_INITIALIZE_FORK_SAFETY'] = 'YES'
-
 import multiprocessing
 from concurrent.futures import (
     ProcessPoolExecutor,
@@ -52,6 +47,14 @@ from c7n_org.utils import environ, account_tags
 from c7n.utils import UnicodeWriter
 
 log = logging.getLogger('c7n_org')
+
+# Workaround OSX issue, note this exists for py2 but there
+# isn't anything we can do in that case.
+# https://bugs.python.org/issue33725
+if sys.platform == 'darwin' and (
+        sys.version_info.major > 3 and sys.version_info.minor > 4):
+    multiprocessing.set_start_method('spawn')
+
 
 WORKER_COUNT = int(
     os.environ.get('C7N_ORG_PARALLEL', multiprocessing.cpu_count() * 4))
