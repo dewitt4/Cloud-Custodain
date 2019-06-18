c7a9bf1b9a3eb7862cbebd6382fc5bed1570ea24
Kapil Thangavelu <kapilt@gmail.com>
2019-06-18 17:13:27 -0400

    release - 0.8.44.1 (#4196)

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 setup(
     name="c7n",
-    version='0.8.44.0',
+    version='0.8.44.1',
     description="Cloud Custodian - Policy Rules Engine",
     long_description=read('README.md'),
     long_description_content_type='text/markdown',
