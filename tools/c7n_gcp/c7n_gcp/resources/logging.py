1637b7b0b4b43c2b4dd9313d66fb3db22927ac60
Dkhanas <dkhanas@gmail.com>
2019-07-31 04:36:02 +0300

    gcp - add stack driver logging initial setup (#3820)

--- tools/c7n_gcp/c7n_gcp/resources/logging.py
+++ tools/c7n_gcp/c7n_gcp/resources/logging.py
@@ -23,15 +23,74 @@ class LogSink(QueryResourceManager):
 
     class resource_type(TypeInfo):
         service = 'logging'
-        version = 'v1'
+        version = 'v2'
         component = 'projects.sinks'
         enum_spec = ('list', 'sinks[]', None)
         scope_key = 'parent'
-        scope_template = "projects/{}/sinks"
+        scope_template = "projects/{}"
         id = "name"
 
         @staticmethod
         def get(client, resource_info):
-            return client.get('get', {
+            return client.execute_query('get', {
                 'sinkName': 'projects/{project_id}/sinks/{name}'.format(
+                    **resource_info)})
+
+
+@resources.register('log-project-sink')
+class LogProjectSink(QueryResourceManager):
+
+    class resource_type(TypeInfo):
+        service = 'logging'
+        version = 'v2'
+        component = 'projects.sinks'
+        enum_spec = ('list', 'sinks[]', None)
+        scope_key = 'parent'
+        scope_template = 'projects/{}'
+        id = 'name'
+
+        @staticmethod
+        def get(client, resource_info):
+            return client.execute_query('get', {
+                'sinkName': 'projects/{project_id}/sinks/{name}'.format(
+                    **resource_info)})
+
+
+@resources.register('log-project-metric')
+class LogProjectMetric(QueryResourceManager):
+
+    class resource_type(TypeInfo):
+        service = 'logging'
+        version = 'v2'
+        component = 'projects.metrics'
+        enum_spec = ('list', 'metrics[]', None)
+        scope_key = 'parent'
+        scope_template = 'projects/{}'
+        id = 'name'
+
+        @staticmethod
+        def get(client, resource_info):
+            return client.execute_query('get', {
+                'metricName': 'projects/{}/metrics/{}'.format(
+                    resource_info['project_id'],
+                    resource_info['name'].split('/')[-1],
+                )})
+
+
+@resources.register('log-exclusion')
+class LogExclusion(QueryResourceManager):
+
+    class resource_type(TypeInfo):
+        service = 'logging'
+        version = 'v2'
+        component = 'exclusions'
+        enum_spec = ('list', 'exclusions[]', None)
+        scope_key = 'parent'
+        scope_template = 'projects/{}'
+        id = 'name'
+
+        @staticmethod
+        def get(client, resource_info):
+            return client.execute_query('get', {
+                'name': 'projects/{project_id}/exclusions/{name}'.format(
                     **resource_info)})
