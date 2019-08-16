3e19b549bd845461cfcdee2c871b38a7e806b4b7
PratyushM <pratyush.mishra@capitalone.com>
2019-08-16 10:09:46 -0400

    aws - acm certificate tag actions (#4529)

--- c7n/resources/acm.py
+++ c7n/resources/acm.py
@@ -16,7 +16,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 from c7n.actions import BaseAction
 from c7n.manager import resources
 from c7n.query import QueryResourceManager, DescribeSource, ConfigSource, TypeInfo
-from c7n.tags import universal_augment
+from c7n.tags import universal_augment, register_universal_tags
 from c7n.utils import type_schema, local_session
 
 
@@ -51,6 +51,9 @@ class DescribeCertificate(DescribeSource):
         return universal_augment(
             self.manager,
             super(DescribeCertificate, self).augment(resources))
+
+
+register_universal_tags(Certificate.filter_registry, Certificate.action_registry)
 
 
 @Certificate.action_registry.register('delete')
--- tests/test_acm.py
+++ tests/test_acm.py
@@ -59,3 +59,23 @@ class CertificateTest(BaseTest):
         client = factory().client("acm")
         arn = "arn:aws:acm:us-west-2:644160558196:certificate/b867707e-33c3-4024-b45d-b6133c3b4c05"
         self.assertTrue(client.get_certificate(CertificateArn=arn))
+
+    def test_certificate_tag_untag_mark(self):
+        factory = self.replay_flight_data("test_certificate_tag_untag_mark")
+        p = self.load_policy(
+            {
+                "name": "acm-tag",
+                "resource": "acm-certificate",
+                "filters": [{"tag:target-tag": "present"}],
+                "actions": [
+                    {"type": "remove-tag", "tags": ["target-tag"]},
+                    {"type": "mark-for-op", "tag": "custodian_cleanup", "op": "delete", "days": 1}],
+            },
+            session_factory=factory,
+        )
+        resources = p.run()
+        self.assertEqual(len(resources), 1)
+        client = factory().client("acm")
+        tag = client.list_tags_for_certificate(CertificateArn=resources[0]['CertificateArn'])
+        self.assertEqual(len(tag.get('Tags')), 1)
+        self.assertEqual(tag.get('Tags')[0]['Key'], "custodian_cleanup")
