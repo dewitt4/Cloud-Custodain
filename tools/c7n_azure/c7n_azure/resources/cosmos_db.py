d115940ed3d247843ae04ea4618f92b32fcba4ef
erwelch <38870842+erwelch@users.noreply.github.com>
2019-08-21 13:16:16 -0700

    azure - cosmos db throughput state (#4639)

--- tools/c7n_azure/c7n_azure/resources/cosmos_db.py
+++ tools/c7n_azure/c7n_azure/resources/cosmos_db.py
@@ -11,33 +11,35 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 import logging
-from concurrent.futures import as_completed
 from itertools import groupby
 
 import azure.mgmt.cosmosdb
 from azure.cosmos.cosmos_client import CosmosClient
 from azure.cosmos.errors import HTTPFailure
-from netaddr import IPSet
-
-from c7n.filters import ValueFilter
-from c7n.utils import type_schema
 from c7n_azure import constants
 from c7n_azure.actions.base import AzureBaseAction
 from c7n_azure.filters import FirewallRulesFilter
 from c7n_azure.provider import resources
 from c7n_azure.query import ChildResourceManager, ChildTypeInfo
 from c7n_azure.resources.arm import ArmResourceManager
+from c7n_azure.tags import TagHelper
 from c7n_azure.utils import ResourceIdParser
+from concurrent.futures import as_completed
+from netaddr import IPSet
+
+from c7n.filters import ValueFilter
+from c7n.utils import type_schema
 
 try:
     from functools import lru_cache
 except ImportError:
     from backports.functools_lru_cache import lru_cache
 
-
 max_workers = constants.DEFAULT_MAX_THREAD_WORKERS
 log = logging.getLogger('azure.cosmosdb')
+THROUGHPUT_MULTIPLIER = 100
 
 
 @resources.register('cosmosdb')
@@ -76,6 +78,26 @@ class CosmosDB(ArmResourceManager):
             'kind'
         )
         resource_type = 'Microsoft.DocumentDB/databaseAccounts'
+
+
+@CosmosDB.filter_registry.register('firewall-rules')
+class CosmosDBFirewallRulesFilter(FirewallRulesFilter):
+
+    def __init__(self, data, manager=None):
+        super(CosmosDBFirewallRulesFilter, self).__init__(data, manager)
+        self._log = logging.getLogger('custodian.azure.cosmosdb')
+
+    @property
+    def log(self):
+        return self._log
+
+    def _query_rules(self, resource):
+
+        ip_range_string = resource['properties']['ipRangeFilter']
+
+        resource_rules = IPSet(ip_range_string.split(','))
+
+        return resource_rules
 
 
 class CosmosDBChildResource(ChildResourceManager):
@@ -163,6 +185,9 @@ class CosmosDBCollection(CosmosDBChildResource):
 
     """
 
+    def tag_operation_enabled(self, resource_type):
+        return True
+
     def enumerate_resources(self, parent_resource, type_info, **params):
         data_client = self.get_data_client(parent_resource)
 
@@ -182,82 +207,10 @@ class CosmosDBCollection(CosmosDBChildResource):
             for c in container_result:
                 c.update({'c7n:document-endpoint':
                          parent_resource.get('properties').get('documentEndpoint')})
+                c['c7n:parent'] = parent_resource
                 collections.append(c)
 
         return collections
-
-
-class OfferHelper(object):
-
-    @staticmethod
-    def account_key(resource):
-        return resource['c7n:document-endpoint']
-
-    @staticmethod
-    def group_by_account(resources):
-        # Group all resources by account because offers are queried per account not per collection
-        account_sorted = sorted(resources, key=OfferHelper.account_key)
-        account_grouped = [list(it) for k, it in groupby(
-            account_sorted,
-            OfferHelper.account_key)]
-
-        return account_grouped
-
-    @staticmethod
-    def get_cosmos_data_client(resources, manager, readonly=True):
-        cosmos_db_key = resources[0]['c7n:parent-id']
-        url_connection = resources[0]['c7n:document-endpoint']
-
-        # Get the data client keys
-        key = CosmosDBChildResource.get_cosmos_key(
-            ResourceIdParser.get_resource_group(cosmos_db_key),
-            ResourceIdParser.get_resource_name(cosmos_db_key),
-            manager.get_client(),
-            readonly
-        )
-
-        # Build a data client
-        data_client = CosmosClient(url_connection=url_connection, auth={'masterKey': key})
-        return data_client
-
-    @staticmethod
-    def populate_offer_data(resources, manager, data_client=None):
-        # Skip if offer key is present anywhere because we already
-        # queried and joined offers in a previous filter instance
-        if not resources[0].get('c7n:offer'):
-
-            if not data_client:
-                data_client = OfferHelper.get_cosmos_data_client(resources, manager)
-
-            # Get the offers
-            offers = list(data_client.ReadOffers())
-
-            # Match up offers to collections
-            for resource in resources:
-                offer = [o for o in offers if o['resource'] == resource['_self']]
-                resource['c7n:offer'] = offer
-
-    @staticmethod
-    def execute_in_parallel_grouped_by_account(
-            resources, executor_factory, process_resource_set, log):
-        futures = []
-        results = []
-        account_grouped = OfferHelper.group_by_account(resources)
-
-        # Process database groups in parallel
-        with executor_factory(max_workers=3) as w:
-            for resource_set in account_grouped:
-                futures.append(w.submit(process_resource_set, resource_set))
-
-            for f in as_completed(futures):
-                if f.exception():
-                    log.warning(
-                        "CosmosDB offer processing error: %s" % f.exception())
-                    continue
-                else:
-                    results.extend(f.result())
-
-            return results
 
 
 @CosmosDBCollection.filter_registry.register('offer')
@@ -275,7 +228,7 @@ class CosmosDBOfferFilter(ValueFilter):
     .. code-block:: yaml
 
         policies:
-          - name: cosmosdb-high-throughput
+          - name: cosmosdb-collection-high-throughput
             resource: azure.cosmosdb-collection
             filters:
               - type: offer
@@ -292,20 +245,19 @@ class CosmosDBOfferFilter(ValueFilter):
         return OfferHelper.execute_in_parallel_grouped_by_account(
             resources,
             self.executor_factory,
-            self.process_resource_set,
+            self.manager.get_parent_manager(),
+            self._process_account_set,
             self.log
         )
 
-    def process_resource_set(self, resources):
+    def _process_account_set(self, resources, data_client):
         matched = []
 
         try:
-            OfferHelper.populate_offer_data(resources, self.manager.get_parent_manager())
-
             # Pass each resource through the base filter
             for resource in resources:
                 filtered_resource = super(CosmosDBOfferFilter, self).process(
-                    resource['c7n:offer'],
+                    [resource['c7n:offer']],
                     event=None)
 
                 if filtered_resource:
@@ -355,54 +307,227 @@ class CosmosDBReplaceOfferAction(AzureBaseAction):
         OfferHelper.execute_in_parallel_grouped_by_account(
             resources,
             self.executor_factory,
-            self._process_resource_set,
-            self.log
+            self.manager.get_parent_manager(),
+            self._process_account_set,
+            self.log,
+            readonly=False
         )
 
-    def _process_resource_set(self, resources):
+    def _process_account_set(self, resources, account_client):
         try:
-
-            # The offer data may not already be available
-            manager = self.manager.get_parent_manager()
-            data_client = OfferHelper.get_cosmos_data_client(resources, manager, readonly=False)
-            OfferHelper.populate_offer_data(resources, manager, data_client)
-
-            # Working under the assumption that there is 1 offer per collection...
-            offer = resources[0]['c7n:offer'][0]
-            new_offer = dict(offer)
-            new_offer.pop('c7n:MatchedFilters', None)
-            new_offer['content']['offerThroughput'] = self.data['throughput']
-            new_offer = data_client.ReplaceOffer(offer['_self'], new_offer)
+            throughput = self.data.get('throughput')
             for resource in resources:
-                resource['c7n:offer'] = [new_offer]
+                self._process_resource(resource, account_client, throughput)
 
         except Exception as e:
             log.warn(e)
 
         return resources
 
+    def _process_resource(self, resource, account_client, throughput):
+        offer = resource['c7n:offer']
+        new_offer = dict(offer)
+        new_offer.pop('c7n:MatchedFilters', None)
+        new_offer['content']['offerThroughput'] = throughput
+        account_client.ReplaceOffer(offer['_self'], new_offer)
+
+
+@CosmosDBCollection.action_registry.register('restore-throughput-state')
+class CosmosDBRestoreStateAction(CosmosDBReplaceOfferAction):
+    """CosmosDB Restore State Action
+
+    Restores the throughput of a cosmodb collection's offer from state
+    stored in a tag on the collections's parent CosmosDB account.
+
+    :example:
+
+    This policy will restore the state of Cosmos DB collections by retrieving the state from
+    the tag 'on-hour-state' from its associated Cosmos DB account.
+
+    .. code-block:: yaml
+
+        policies:
+          - name: restore-throughput-state
+            resource: azure.cosmosdb-collection
+            actions:
+              - type: restore-throughput-state
+                state-tag: on-hour-state
+
+    """
+
+    schema = type_schema(
+        'restore-throughput-state',
+        required=['state-tag'],
+        **{
+            'state-tag': {'type': 'string'}
+        }
+    )
+
+    def _process_account_set(self, resources, account_client):
+        try:
+            parent_account = resources[0]['c7n:parent']
+            tag_name = self.data.get('state-tag')
+            container_states_tag_value = TagHelper.get_tag_value(
+                parent_account, tag_name)
+
+            if container_states_tag_value:
+                for state in container_states_tag_value.split(';'):
+                    state_data = state.split(':')
+                    container_rid = state_data[0]
+                    # restoring throughput size with multiplier since it was stored to save space
+                    container_throughput = int(state_data[1]) * THROUGHPUT_MULTIPLIER
+
+                    container = next((c for c in resources if c['_rid'] == container_rid), None)
+
+                    if container:
+                        self._process_resource(container, account_client, container_throughput)
+            else:
+                self.log.warning('No tag {} on parent resource, {}.'.format(
+                    tag_name, parent_account))
+
+        except Exception as e:
+            log.warn(e)
+
+        return resources
+
+
+@CosmosDBCollection.action_registry.register('save-throughput-state')
+class CosmosDBSaveStateAction(AzureBaseAction):
+    """CosmosDB Store State Action
+
+    Stores the throughput of collections in a tag on the parent Cosmos DB account.
+    With accounts that have many collections, it's important to filter down which
+    collections to store since there is a tag length limit (approx 16 collections).
+
+    :example:
+
+    This policy saves the throughput of collections with throughput over 400 in
+    a tag called 'on-hour-state' on the parent Cosmos DB account.
+
+    .. code-block:: yaml
+
+        policies:
+          - name: store-on-hours-state
+            resource: azure.cosmosdb-collection
+            filters:
+              - type: offer
+                key: content.offerThroughput
+                op: gt
+                value: 400
+            actions:
+              - type: save-throughput-state
+                state-tag: on-hour-state
+
+    """
+
+    schema = type_schema(
+        'save-throughput-state',
+        required=['state-tag'],
+        **{
+            'state-tag': {'type': 'string'}
+        }
+    )
+
+    TAG_VALUE_CHAR_LIMIT = 256
+
+    def _process_resources(self, resources, event):
+        OfferHelper.execute_in_parallel_grouped_by_account(
+            resources,
+            self.executor_factory,
+            self.manager.get_parent_manager(),
+            self._process_account_set,
+            self.log
+        )
+
+    def _process_account_set(self, resources, account_client):
+        account_tag_values = []
+        tag_name = self.data.get('state-tag')
+        cosmos_account = resources[0]['c7n:parent']
+
+        for resource in resources:
+            # dividing by multiplier to reduce string size (throughputs are multiples of 100)
+            throughput = int(
+                resource['c7n:offer']['content']['offerThroughput'] / THROUGHPUT_MULTIPLIER)
+
+            account_tag_values.append('{}:{}'.format(
+                resource['_rid'], throughput))
+
+        tag_value = ';'.join(account_tag_values)
+
+        if len(tag_value) > self.TAG_VALUE_CHAR_LIMIT:
+            raise ValueError('Can not add tag, {}, on parent resource, {}, '
+                             'because tag value exceeds allowed length.'
+                             'Add filters to reduce number of containers.'
+                             .format(tag_name, cosmos_account['name']))
+
+        TagHelper.add_tags(self, cosmos_account, {tag_name: tag_value})
+        return resources
+
     def _process_resource(self, resource):
-        # Since the offer lives on the account, not the collection, this action does not
-        # apply to resources individually
-        raise NotImplementedError(
-            "CosmosDBReplaceOfferAction processes resources as a group, not individually")
+        pass
 
 
-@CosmosDB.filter_registry.register('firewall-rules')
-class CosmosDBFirewallRulesFilter(FirewallRulesFilter):
+class OfferHelper(object):
 
-    def __init__(self, data, manager=None):
-        super(CosmosDBFirewallRulesFilter, self).__init__(data, manager)
-        self._log = logging.getLogger('custodian.azure.cosmosdb')
+    @staticmethod
+    def account_key(resource):
+        return resource['c7n:document-endpoint']
 
-    @property
-    def log(self):
-        return self._log
+    @staticmethod
+    def group_by_account(resources):
+        # Group all resources by account because offers are queried per account not per collection
+        account_sorted = sorted(resources, key=OfferHelper.account_key)
+        account_grouped = [list(it) for k, it in groupby(
+            account_sorted,
+            OfferHelper.account_key)]
 
-    def _query_rules(self, resource):
+        return account_grouped
 
-        ip_range_string = resource['properties']['ipRangeFilter']
+    @staticmethod
+    def get_cosmos_data_client_for_account(account_id, account_endpoint, manager, readonly=True):
+        key = CosmosDBChildResource.get_cosmos_key(
+            ResourceIdParser.get_resource_group(account_id),
+            ResourceIdParser.get_resource_name(account_id),
+            manager.get_client(),
+            readonly
+        )
 
-        resource_rules = IPSet(ip_range_string.split(','))
+        data_client = CosmosClient(url_connection=account_endpoint, auth={'masterKey': key})
+        return data_client
 
-        return resource_rules
+    @staticmethod
+    def populate_offer_data_for_account(resources, account_data_client):
+        if not resources[0].get('c7n:offer'):
+            offers = list(account_data_client.ReadOffers())
+            for resource in resources:
+                offer = next((o for o in offers if o['resource'] == resource['_self']), None)
+                resource['c7n:offer'] = offer
+
+    @staticmethod
+    def execute_in_parallel_grouped_by_account(
+            resources, executor_factory, account_manager, process_resource_set, log, readonly=True):
+        futures = []
+        results = []
+        account_grouped = OfferHelper.group_by_account(resources)
+
+        # Process cosmos db account groups in parallel
+        with executor_factory(max_workers=3) as w:
+            for resource_set in account_grouped:
+
+                account_client = OfferHelper.get_cosmos_data_client_for_account(
+                    resource_set[0]['c7n:parent-id'],
+                    resource_set[0]['c7n:document-endpoint'],
+                    account_manager, readonly)
+
+                OfferHelper.populate_offer_data_for_account(resource_set, account_client)
+                futures.append(w.submit(process_resource_set, resource_set, account_client))
+
+            for f in as_completed(futures):
+                if f.exception():
+                    log.warning(
+                        "CosmosDB offer processing error: %s" % f.exception())
+                    continue
+                else:
+                    results.extend(f.result())
+
+            return results
--- tools/c7n_azure/tests/test_cosmos_db.py
+++ tools/c7n_azure/tests/test_cosmos_db.py
@@ -148,30 +148,65 @@ class CosmosDBTest(BaseTest):
         self.assertEqual(1, len(resources))
         self.assertEqual('Hash', resources[0]['partitionKey']['kind'])
 
+    @arm_template('cosmosdb.json')
+    def test_store_throughput_state_collection_action(self):
+        p = self.load_policy({
+            'name': 'test-azure-cosmosdb',
+            'resource': 'azure.cosmosdb-collection',
+            'filters': [
+                {
+                    'type': 'value',
+                    'key': 'id',
+                    'op': 'eq',
+                    'value': 'cccontainer'
+                }
+            ],
+            'actions': [
+                {
+                    'type': 'save-throughput-state',
+                    'state-tag': 'test-store-throughput'
+                }
+            ]
+        })
 
-class CosmosDBReplaceOfferActionTest(BaseTest):
+        collections = p.run()
+        self.assertEqual(len(collections), 1)
+
+        client = local_session(Session).client('azure.mgmt.cosmosdb.CosmosDB')
+        cosmos_account = client.database_accounts.get('test_cosmosdb', 'cctestcosmosdb')
+        self.assertTrue('test-store-throughput' in cosmos_account.tags)
+
+        tag_value = cosmos_account.tags['test-store-throughput']
+        expected_throughput = collections[0]['c7n:offer']['content']['offerThroughput']
+        expected_tag_value = '{}:{}'.format(collections[0]['_rid'], expected_throughput)
+        self.assertEqual(expected_tag_value, tag_value)
+
+
+class CosmosDBThroughputActionsTest(BaseTest):
 
     def setUp(self, *args, **kwargs):
-        super(CosmosDBReplaceOfferActionTest, self).setUp(*args, **kwargs)
-        client = local_session(Session).client('azure.mgmt.cosmosdb.CosmosDB')
+        super(CosmosDBThroughputActionsTest, self).setUp(*args, **kwargs)
+        self.client = local_session(Session).client('azure.mgmt.cosmosdb.CosmosDB')
         key = CosmosDBChildResource.get_cosmos_key(
-            'test_cosmosdb', 'cctestcosmosdb', client, readonly=False)
+            'test_cosmosdb', 'cctestcosmosdb', self.client, readonly=False)
         self.data_client = CosmosClient(
             url_connection='https://cctestcosmosdb.documents.azure.com:443/',
             auth={
                 'masterKey': key
             }
         )
+        self.offer = None
 
     def tearDown(self, *args, **kwargs):
-        super(CosmosDBReplaceOfferActionTest, self).tearDown(*args, **kwargs)
-        self.data_client.ReplaceOffer(
-            self.initial_offer['_self'],
-            self.initial_offer
-        )
+        super(CosmosDBThroughputActionsTest, self).tearDown(*args, **kwargs)
+        if self.offer:
+            self.offer['content']['offerThroughput'] = 400
+            self.data_client.ReplaceOffer(
+                self.offer['_self'],
+                self.offer
+            )
 
     def test_replace_offer_collection_action(self):
-
         p = self.load_policy({
             'name': 'test-azure-cosmosdb',
             'resource': 'azure.cosmosdb-collection',
@@ -197,11 +232,72 @@ class CosmosDBReplaceOfferActionTest(BaseTest):
             ]
         })
         collections = p.run()
+        self.offer = collections[0]['c7n:offer']
+
+        self.assertEqual(len(collections), 1)
+        self._assert_offer_throughput_equals(500, collections[0]['_self'])
+
+    def test_restore_throughput_state_updates_throughput_from_tag(self):
+
+        p1 = self.load_policy({
+            'name': 'test-azure-cosmosdb',
+            'resource': 'azure.cosmosdb-collection',
+            'filters': [
+                {
+                    'type': 'value',
+                    'key': 'id',
+                    'op': 'eq',
+                    'value': 'cccontainer'
+                }
+            ],
+            'actions': [
+                {
+                    'type': 'save-throughput-state',
+                    'state-tag': 'test-restore-throughput'
+                }
+            ]
+        })
+
+        collections = p1.run()
         self.assertEqual(len(collections), 1)
 
-        self.initial_offer = collections[0]['c7n:offer'][0]
-        self.collection = collections[0]
-        self._assert_offer_throughput_equals(500, collections[0]['_self'])
+        collection_offer = collections[0]['c7n:offer']
+        self.offer = collection_offer
+
+        throughput_to_restore = collection_offer['content']['offerThroughput']
+
+        collection_offer['content']['offerThroughput'] = throughput_to_restore + 100
+
+        self.data_client.ReplaceOffer(
+            collection_offer['_self'],
+            collection_offer
+        )
+
+        self._assert_offer_throughput_equals(throughput_to_restore + 100, collections[0]['_self'])
+
+        p2 = self.load_policy({
+            'name': 'test-azure-cosmosdb',
+            'resource': 'azure.cosmosdb-collection',
+            'filters': [
+                {
+                    'type': 'value',
+                    'key': 'id',
+                    'op': 'eq',
+                    'value': 'cccontainer'
+                },
+            ],
+            'actions': [
+                {
+                    'type': 'restore-throughput-state',
+                    'state-tag': 'test-restore-throughput'
+                }
+            ]
+        })
+
+        collections = p2.run()
+
+        self.assertEqual(len(collections), 1)
+        self._assert_offer_throughput_equals(throughput_to_restore, collections[0]['_self'])
 
     def _assert_offer_throughput_equals(self, throughput, resource_self):
         offers = self.data_client.ReadOffers()
