7e4ef64cf67bc3db0e4eec5491c6667ed44b35fb
Andy Luong <anluong@microsoft.com>
2019-09-13 15:07:11 -0700

    azure - update provisioning of win consumption app service plans (#4789)

--- tools/c7n_azure/tests/test_app_service_plan.py
+++ tools/c7n_azure/tests/test_app_service_plan.py
@@ -62,6 +62,7 @@ class AppServicePlanTest(BaseTest):
     @patch('azure.mgmt.web.operations.app_service_plans_operations.'
            'AppServicePlansOperations.update')
     @arm_template('appserviceplan.json')
+    @cassette_name('window_plans')
     def test_resize_plan_win(self, update_mock):
         p = self.load_policy({
             'name': 'test-azure-appserviceplan-win',
@@ -94,6 +95,7 @@ class AppServicePlanTest(BaseTest):
     @patch('azure.mgmt.web.operations.app_service_plans_operations.'
            'AppServicePlansOperations.update')
     @arm_template('appserviceplan-linux.json')
+    @cassette_name('linux_plans')
     def test_resize_plan_linux(self, update_mock):
         p = self.load_policy({
             'name': 'test-azure-appserviceplan-linux',
@@ -126,7 +128,7 @@ class AppServicePlanTest(BaseTest):
     @patch('azure.mgmt.web.operations.app_service_plans_operations.'
            'AppServicePlansOperations.update')
     @arm_template('appserviceplan.json')
-    @cassette_name('test_resize_plan_win')
+    @cassette_name('window_plans')
     def test_resize_plan_from_resource_tag(self, update_mock):
         p = self.load_policy({
             'name': 'test-azure-appserviceplan',
@@ -154,6 +156,7 @@ class AppServicePlanTest(BaseTest):
 
     @arm_template('appserviceplan.json')
     @patch('c7n_azure.resources.appserviceplan.ResizePlan.log.info')
+    @cassette_name('window_plans')
     def test_resize_consumption_win(self, logger):
         p = self.load_policy({
             'name': 'test-azure-consumption-win',
@@ -177,6 +180,7 @@ class AppServicePlanTest(BaseTest):
 
     @arm_template('appserviceplan-linux.json')
     @patch('c7n_azure.resources.appserviceplan.ResizePlan.log.info')
+    @cassette_name('linux_plans')
     def test_resize_consumption_linux(self, logger):
         p = self.load_policy({
             'name': 'test-azure-appserviceplan-linux',
@@ -201,7 +205,7 @@ class AppServicePlanTest(BaseTest):
     @patch('azure.mgmt.web.operations.app_service_plans_operations.'
            'AppServicePlansOperations.update')
     @arm_template('appserviceplan.json')
-    @cassette_name('test_resize_plan_win')
+    @cassette_name('window_plans')
     def test_resize_plan_win_only_count(self, update_mock):
         p = self.load_policy({
             'name': 'test-azure-appserviceplan-win',
