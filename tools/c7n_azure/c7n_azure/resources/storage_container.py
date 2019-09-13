d14133502ad40230f25ea58040877e2e49f6510e
Stefan Gordon <stefan@stefangordon.com>
2019-09-13 11:08:12 -0700

    azure - skip failed container enumerations (#4784)

--- tools/c7n_azure/c7n_azure/resources/storage_container.py
+++ tools/c7n_azure/c7n_azure/resources/storage_container.py
@@ -50,6 +50,7 @@ class StorageContainer(ChildResourceManager):
         diagnostic_settings_enabled = False
         resource_type = 'Microsoft.Storage/storageAccounts/blobServices/containers'
         enable_tag_operations = False
+        raise_on_exception = False
 
         @classmethod
         def extra_args(cls, parent_resource):
