8daa757c314672ae417edc838e188da95de08228
Sonny <theli@outlook.com>
2019-09-26 12:33:53 -0700

    aws.rds - modify sg action fix missing vpcid expression when using sg names (#4867)

--- c7n/resources/rds.py
+++ c7n/resources/rds.py
@@ -1343,6 +1343,7 @@ class RDSSnapshotDelete(BaseAction):
 class RDSModifyVpcSecurityGroups(ModifyVpcSecurityGroupsAction):
 
     permissions = ('rds:ModifyDBInstance', 'rds:ModifyDBCluster')
+    vpc_expr = 'DBSubnetGroup.VpcId'
 
     def process(self, rds_instances):
         replication_group_map = {}
