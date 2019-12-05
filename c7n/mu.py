e8c38d61f92142828cfebc0d77962e4ba6ef6dfd
myoung34 <myoung34@my.apsu.edu>
2019-12-05 07:06:46 -0600

    aws - phd mode - fix all events support (#5133)

--- c7n/mu.py
+++ c7n/mu.py
@@ -1086,8 +1086,10 @@ class CloudWatchEventSource(object):
             payload['detail-type'] = events
         elif event_type == 'phd':
             payload['source'] = ['aws.health']
-            payload['detail'] = {
-                'eventTypeCode': list(self.data['events'])}
+            if self.data.get('events'):
+                payload['detail'] = {
+                    'eventTypeCode': list(self.data['events'])
+                }
             if self.data.get('categories', []):
                 payload['detail']['eventTypeCategory'] = self.data['categories']
         elif event_type == 'hub-finding':
