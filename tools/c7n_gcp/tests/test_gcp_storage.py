e5016796cb1fcdacde83b79fdddf7b14e184d415
Allison D <45855079+allisonis@users.noreply.github.com>
2020-05-09 02:29:05 -0700

    gcp - support for enabling uniform bucket access (#5688)

--- tools/c7n_gcp/tests/test_gcp_storage.py
+++ tools/c7n_gcp/tests/test_gcp_storage.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import time
+
 from gcp_common import BaseTest
 
 
@@ -43,3 +45,30 @@ class BucketTest(BaseTest):
         self.assertEqual(bucket['id'], "staging.cloud-custodian.appspot.com")
         self.assertEqual(bucket['storageClass'], "STANDARD")
         self.assertEqual(bucket['location'], "EU")
+
+    def test_enable_uniform_bucket_level_access(self):
+        project_id = 'custodian-1291'
+        bucket_name = 'c7n-dev-test'
+        factory = self.replay_flight_data(
+            'bucket-uniform-bucket-access', project_id)
+        p = self.load_policy({
+            'name': 'bucket',
+            'resource': 'gcp.bucket',
+            'filters': [
+                {'name': 'c7n-dev-test'},
+                {'iamConfiguration.uniformBucketLevelAccess.enabled': False},
+            ],
+            'actions': ['set-uniform-access']},
+            session_factory=factory)
+        resources = p.run()
+        self.assertEqual(len(resources), 1)
+        if self.recording:
+            time.sleep(5)
+        bucket = p.resource_manager.get_resource({
+            "bucket_name": bucket_name,
+        })
+        self.assertEqual(bucket['name'], bucket_name)
+        self.assertEqual(bucket['id'], bucket_name)
+        self.assertEqual(bucket['storageClass'], "REGIONAL")
+        self.assertEqual(bucket['location'], "US-EAST1")
+        self.assertJmes('iamConfiguration.uniformBucketLevelAccess.enabled', bucket, True)
