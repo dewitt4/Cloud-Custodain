9d01edf33bed0f584a329d869d80ba885fe49816
Cheryl Sabella <cheryl.sabella@gmail.com>
2020-06-08 13:28:47 -0400

    releng - fix warnings on regex escape sequences (#5843)

--- tests/zpill.py
+++ tests/zpill.py
@@ -271,7 +271,7 @@ class RedPill(pill.Pill):
             response_data['ResponseMetadata'] = {}
 
         response_data = json.dumps(response_data, default=serialize)
-        response_data = re.sub("\d{12}", ACCOUNT_ID, response_data)  # noqa
+        response_data = re.sub(r"\d{12}", ACCOUNT_ID, response_data)  # noqa
         response_data = json.loads(response_data, object_hook=deserialize)
 
         super(RedPill, self).save_response(service, operation, response_data,
